4c9d9dda7e23b1336dd42afac558fbcdc28c1f51
Самсонов Олег <Samsonov@dev.ckrt.ru>
2017-12-17 14:07:21 +0300

    reverse

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 :start
 powershell -version 5.0 -executionpolicy bypass -command "&.\MindMiner.ps1"
 if exist "bin\mm.new" (
-	xcopy Bin\MM.New . /y /s /c /q /exclude:run.bat+.gitignore
+	xcopy Bin\MM.New . /y /s /c /q /exclude:run.bat
 	rmdir /q /s Bin\MM.New
 	goto start:
 ) else (
