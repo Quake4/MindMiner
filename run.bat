b217b6619daa52c7a69f72f806077a8cfcb7a61e
Самсонов Олег <Samsonov@dev.ckrt.ru>
2017-12-17 13:54:08 +0300

    update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 :start
 powershell -version 5.0 -executionpolicy bypass -command "&.\MindMiner.ps1"
 if exist "bin\mm.new" (
-	xcopy Bin\MM.New . /y /s /c /q /exclude:run.bat
+	xcopy Bin\MM.New . /y /s /c /q /exclude:run.bat+.gitignore
 	rmdir /q /s Bin\MM.New
 	goto start:
 ) else (
