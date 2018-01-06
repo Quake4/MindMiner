1f98e74131e27c70f7a5fe6199db7a8dd2257b4f
Quake4 <batsov@yandex.ru>
2018-01-06 15:03:07 +0300

    merge master

--- Code/Get-FormatOutput.ps1
+++ Code/Get-FormatOutput.ps1
@@ -1,8 +1,29 @@
 function Get-FormatMiners {
+
+		# Format-Table @{ Label="Miner"; Expression = {
+		# 		$uniq =  $_.Miner.GetUniqueKey()
+		# 		$str = [string]::Empty
+		# 		($ActiveMiners.Values | Where-Object { $_.State -ne [eState]::Stopped } | ForEach-Object {
+		# 			if ($_.Miner.GetUniqueKey() -eq $uniq) {
+		# 				if ($_.State -eq [eState]::Running) { $str = "*" } elseif ($_.State -eq [eState]::NoHash) { $str = "-" } elseif ($_.State -eq [eState]::Failed) { $str = "!" } } })
+		# 		$str + $_.Miner.Name } },
+		# 	@{ Label="Algorithm"; Expression = { $_.Miner.Algorithm } },
+		# 	@{ Label="Speed, H/s"; Expression = { if ($_.Speed -eq 0) { "Testing" } else { [MultipleUnit]::ToString($_.Speed) } }; Alignment="Right" },
+		# 	@{ Label="mBTC/Day"; Expression = { if ($_.Speed -eq 0) { "$($_.Miner.BenchmarkSeconds) sec" } else { $_.Profit * 1000 } }; FormatString = "N5" },
+		# 	@{ Label="BTC/GH/Day"; Expression = { $_.Price * 1000000000 }; FormatString = "N8" },
+		# 	@{ Label="Pool"; Expression = { $_.Miner.Pool } },
+		# 	@{ Label="ExtraArgs"; Expression = { $_.Miner.ExtraArgs } } -GroupBy @{ Label="Type"; Expression = { $_.Miner.Type } } | Out-Host
+	
 	$AllMinersFormatTable = [Collections.ArrayList]::new()
 
 	$AllMinersFormatTable.AddRange(@(
-		@{ Label="Miner"; Expression = { $_.Miner.Name } }
+		@{ Label="Miner"; Expression = {
+			$uniq =  $_.Miner.GetUniqueKey()
+			$str = [string]::Empty
+			($ActiveMiners.Values | Where-Object { $_.State -ne [eState]::Stopped } | ForEach-Object {
+				if ($_.Miner.GetUniqueKey() -eq $uniq) {
+					if ($_.State -eq [eState]::Running) { $str = "*" } elseif ($_.State -eq [eState]::NoHash) { $str = "-" } elseif ($_.State -eq [eState]::Failed) { $str = "!" } } })
+			$str + $_.Miner.Name } }
 		@{ Label="Algorithm"; Expression = { $_.Miner.Algorithm } }
 		@{ Label="Speed, H/s"; Expression = { if ($_.Speed -eq 0) { "Testing" } else { [MultipleUnit]::ToString($_.Speed) } }; Alignment="Right" }
 	))
