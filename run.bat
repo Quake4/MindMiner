59bc86d4fed9549c046ceb1baed79c3ab4f81010
Quake4 <batsov@yandex.ru>
2018-01-30 21:55:56 +0300

    Fix powershell profile use

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 :start
-powershell -version 5.0 -executionpolicy bypass -command "&.\MindMiner.ps1"
+powershell -version 5.0 -executionpolicy bypass -noprofile -command "&.\MindMiner.ps1"
 if exist "bin\mm.new" (
 	xcopy Bin\MM.New . /y /s /c /q /exclude:run.bat
 	rmdir /q /s Bin\MM.New
