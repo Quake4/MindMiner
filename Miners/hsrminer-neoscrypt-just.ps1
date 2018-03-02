7c282b1d1b3223e31c4a11f5c42166b4a40c9c43
Samsonov <samsonov@ckrt.ru>
2018-03-02 15:44:19 +0300

    test nsrminer fork by justaminer

---
+++ Miners/hsrminer-neoscrypt-just.ps1
@@ -0,0 +1,47 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $false
+	BenchmarkSeconds = 120
+	Algorithms = @(
+	[AlgoInfoEx]@{ Enabled = $true; Algorithm = "neoscrypt" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					API = "ccminer"
+					URI = "https://github.com/justaminer/hsrm-fork/raw/master/hsrminer_neoscrypt_fork.zip"
+					Path = "$Name\hsrminer_neoscrypt_fork.exe"
+					ExtraArgs = $_.ExtraArgs
+					Arguments = "-o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R 5 $($_.ExtraArgs)"
+					Port = 4068
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					Fee = 1.7
+				}
+			}
+		}
+	}
+}
