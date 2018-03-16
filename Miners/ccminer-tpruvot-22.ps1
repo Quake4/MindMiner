979a4fcbb3706e0f20067224fda29ab756353382
Samsonov <samsonov@ckrt.ru>
2018-03-16 10:22:22 +0300

    add ccminer tpruvot 2.2 32bit

---
+++ Miners/ccminer-tpruvot-22.ps1
@@ -0,0 +1,44 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 120
+	Algorithms = @(
+	[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lyra2v2" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					API = "ccminer"
+					URI = "https://github.com/Quake4/MindMinerPrerequisites/raw/master/nVidia/ccminer-tpruvot/ccminer-tpruvot-22.zip"
+					Path = "$Name\ccminer.exe"
+					ExtraArgs = $_.ExtraArgs
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R 5 -N 1 $($_.ExtraArgs)"
+					Port = 4068
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+				}
+			}
+		}
+	}
+}
