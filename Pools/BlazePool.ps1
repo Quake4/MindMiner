6e765fc18954ecca3c587cdd7f0fc3db194732e3
Quake4 <batsov@yandex.ru>
2018-03-18 22:54:08 +0300

    add blazepool

---
+++ Pools/BlazePool.ps1
@@ -0,0 +1,98 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$PoolInfo = [PoolInfo]::new()
+$PoolInfo.Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $PoolInfo.Name + [BaseConfig]::Filename), @{
+	Enabled = $false
+	AverageProfit = "1 hour 30 min"
+})
+if (!$Cfg) { return $PoolInfo }
+if (!$Config.Wallet.BTC) { return $PoolInfo }
+
+$PoolInfo.Enabled = $Cfg.Enabled
+$PoolInfo.AverageProfit = $Cfg.AverageProfit
+
+if (!$Cfg.Enabled) { return $PoolInfo }
+
+[decimal] $Pool_Variety = 0.80
+[decimal] $DifFactor = 1.7
+
+try {
+	$RequestStatus = Get-UrlAsJson "http://api.blazepool.com/status"
+}
+catch { return $PoolInfo }
+
+try {
+	$RequestBalance = Get-UrlAsJson "http://api.blazepool.com/wallet/$($Config.Wallet.BTC)"
+}
+catch { }
+
+if (!$RequestStatus) { return $PoolInfo }
+$PoolInfo.HasAnswer = $true
+$PoolInfo.AnswerTime = [DateTime]::Now
+
+if ($RequestBalance) {
+	$PoolInfo.Balance.Value = [decimal]($RequestBalance.balance)
+	$PoolInfo.Balance.Additional = [decimal]($RequestBalance.unsold)
+}
+
+# if ($Config.SSL -eq $true) { $Pool_Protocol = "stratum+ssl" } else { $Pool_Protocol = "stratum+tcp" }
+
+$RequestStatus | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+	$Pool_Algorithm = Get-Algo($RequestStatus.$_.name)
+	if ($Pool_Algorithm -and $RequestStatus.$_.actual_last24h -ne $RequestStatus.$_.estimate_last24h -and [decimal]$RequestStatus.$_.estimate_current -gt 0) {
+		$Pool_Host = "$($RequestStatus.$_.name).mine.blazepool.com"
+		$Pool_Port = $RequestStatus.$_.port
+
+		$Divisor = 1000000
+		
+		switch ($Pool_Algorithm) {
+			"blake" { $Divisor *= 1000 }
+			"blake2s" { $Divisor *= 1000 }
+			"blakecoin" { $Divisor *= 1000 }
+			"decred" { $Divisor *= 1000 }
+			"equihash" { $Divisor /= 1000 }
+			"keccak" { $Divisor *= 1000 }
+			"keccakc" { $Divisor *= 1000 }
+			"nist5" { $Divisor *= 3 }
+			"qubit" { $Divisor *= 1000 }
+			"x11" { $Divisor *= 1000 }
+			"yescrypt" { $Divisor /= 1000 }
+		}
+
+		# convert to one dimension and decimal
+		$Algo = $RequestStatus.$_
+		$Algo.actual_last24h = [decimal]$Algo.actual_last24h / 1000
+		$Algo.estimate_last24h = [decimal]$Algo.estimate_last24h
+		$Algo.estimate_current = [decimal]$Algo.estimate_current
+		# fix very high or low daily changes
+		if ($Algo.estimate_last24h -gt $Algo.actual_last24h * $DifFactor) { $Algo.estimate_last24h = $Algo.actual_last24h * $DifFactor }
+		if ($Algo.estimate_last24h -gt $Algo.estimate_current * $DifFactor) { $Algo.estimate_last24h = $Algo.estimate_current * $DifFactor }
+
+		$Profit = $Algo.estimate_current * [Config]::CurrentOf24h + $Algo.estimate_last24h * (1 - [Config]::CurrentOf24h)
+		$Profit = $Profit * (1 - [decimal]$Algo.fees / 100) * $Pool_Variety / $Divisor
+		$Profit = Set-Stat -Filename ($PoolInfo.Name) -Key $Pool_Algorithm -Value $Profit -Interval $Cfg.AverageProfit
+
+		$PoolInfo.Algorithms.Add([PoolAlgorithmInfo] @{
+			Name = $PoolInfo.Name
+			Algorithm = $Pool_Algorithm
+			Profit = $Profit
+			Info = $MaxCoin.Coin
+			Protocol = "stratum+tcp" # $Pool_Protocol
+			Host = $Pool_Host
+			Port = $Pool_Port
+			PortUnsecure = $Pool_Port
+			User = $Config.Wallet.BTC
+			Password = "c=BTC,$($Config.WorkerName)" # "c=$($MaxCoin.Coin),$($Config.WorkerName)";
+		})
+	}
+}
+
+$PoolInfo
