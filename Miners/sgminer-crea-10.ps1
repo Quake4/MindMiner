f12a479474260ae66eb97aa4366aff3560058a3e
Quake4 <batsov@yandex.ru>
2018-03-20 00:26:16 +0300

    add sgminer for algo keccakc and coin crea

---
+++ Miners/sgminer-crea-10.ps1
@@ -0,0 +1,45 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 60
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "keccakc" }
+	)
+})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "sgminer"
+					URI = "https://www.creativechain.org/wp-content/uploads/2017/10/SG-Miner-CREA-Win.zip"
+					Path = "$Name\sgminer.exe"
+					ExtraArgs = $_.ExtraArgs
+					Arguments = "-k $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-listen --gpu-platform $([Config]::AMDPlatformId) $($_.ExtraArgs)"
+					Port = 4028
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+				}
+			}
+		}
+	}
+}
