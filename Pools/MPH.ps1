29e999ce0702eddf278c3a1ef8670226ff91e1b3
Samsonov <samsonov@ckrt.ru>
2018-03-22 14:11:57 +0300

    remove startcoin from mph

--- Pools/MPH.ps1
+++ Pools/MPH.ps1
@@ -22,7 +22,7 @@ $PoolInfo.AverageProfit = $Cfg.AverageProfit
 
 if (!$Cfg.Enabled) { return $PoolInfo }
 $Pool_Variety = 0.85
-$NoExchangeCoins = @("Electroneum", "Geocoin", "Maxcoin", "Sexcoin")
+$NoExchangeCoins = @("Electroneum", "Geocoin", "Maxcoin", "Sexcoin", "Startcoin")
 
 try {
 	$Request = Get-UrlAsJson "http://miningpoolhub.com/index.php?page=api&action=getminingandprofitsstatistics"
