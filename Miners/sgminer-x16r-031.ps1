bb913c5c31d5748fb98b0bc7aaf2d95ba8444e68
Samsonov <samsonov@ckrt.ru>
2018-03-26 11:26:23 +0300

    Add sgminer for algo X16R

---
+++ Miners/sgminer-x16r-031.ps1
@@ -0,0 +1,47 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 60
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r"; BenchmarkSeconds = 180; ExtraArgs="-I 18" } # build
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r"; ExtraArgs="-I 19" } #570/580
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r"; ExtraArgs="-I 21" } #vega?
+	)
+})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "sgminer"
+					URI = "https://github.com/brian112358/sgminer-x16r/releases/download/v0.3.1-dev/sgminer-x16r-v0.3.1-dev-windows.zip"
+					Path = "$Name\sgminer.exe"
+					ExtraArgs = $_.ExtraArgs
+					Arguments = "-k $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-listen --gpu-platform $([Config]::AMDPlatformId) $($_.ExtraArgs)"
+					Port = 4028
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+				}
+			}
+		}
+	}
+}
