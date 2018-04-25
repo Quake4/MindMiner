919be298a4c2018f3d0a4553af4965d1999dfc7f
Samsonov <samsonov@ckrt.ru>
2018-04-25 11:19:59 +0300

    add skunk to stat avg

--- Code/Include.ps1
+++ Code/Include.ps1
@@ -96,8 +96,8 @@ function Get-CCMinerStatsAvg (
 	[Parameter(Mandatory)] $info # AlgoInfo or AlgoInfoEx
 ) {
 	[hashtable] $vals = @{
-		"Bitcore" = "-N 3"; "C11" = "-N 3"; "Hsr" = "-N 3"; "Phi" = "-N 1"; "Skein" = "-N 3"; "Tribus" = "-N 1";
-		"Lyra2re2" = "-N 1"; "Lyra2z" = "-N 1"; "X16r" = "-N 3"; "X16s" = "-N 3"; "X17" = "-N 1"
+		"Bitcore" = "-N 3"; "C11" = "-N 3"; "Hsr" = "-N 3"; "Phi" = "-N 1"; "Skein" = "-N 3"; "Skunk" = "-N 5";
+		"Tribus" = "-N 1"; "Lyra2re2" = "-N 1"; "Lyra2z" = "-N 1"; "X16r" = "-N 3"; "X16s" = "-N 3"; "X17" = "-N 1"
 	}
 
 	if (!$algo -or !$info) { [ArgumentNullException]::new("Get-CCMinerStatsAvg") }
