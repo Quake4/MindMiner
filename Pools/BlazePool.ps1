959fb92b4d2d24f2d3b587dc5308f91e37557d78
Samsonov <samsonov@ckrt.ru>
2018-05-14 10:34:59 +0300

    change order in id and coin at blazepool

--- Pools/BlazePool.ps1
+++ Pools/BlazePool.ps1
@@ -98,7 +98,7 @@ $RequestStatus | Get-Member -MemberType NoteProperty | Select-Object -ExpandProp
 			Port = $Pool_Port
 			PortUnsecure = $Pool_Port
 			User = $Config.Wallet.BTC
-			Password = Get-Join "," @("ID=$([Config]::WorkerNamePlaceholder)","c=BTC", $Pool_Diff)
+			Password = Get-Join "," @("c=BTC", "ID=$([Config]::WorkerNamePlaceholder)", $Pool_Diff)
 		})
 	}
 }
