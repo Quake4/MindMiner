045badf6e08b407e4885da8eafdab3f579231f44
Samsonov <samsonov@ckrt.ru>
2018-05-27 21:29:14 +0300

    update sgminer-kl to version 1.0.4

--- Miners/sgminer-kl-104.ps1
+++ Miners/sgminer-kl-104.ps1
@@ -10,14 +10,15 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
 	Enabled = $true
-	BenchmarkSeconds = 60
+	BenchmarkSeconds = 120
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "phi"; BenchmarkSeconds = 180; ExtraArgs="-X 256 -g 2 -w 256" } # with build
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "tribus"; BenchmarkSeconds = 180; ExtraArgs="-X 256 -g 2" } # with build
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r"; BenchmarkSeconds = 180; ExtraArgs="-X 256 -g 2" } # with build
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16s"; BenchmarkSeconds = 90; ExtraArgs="-X 256 -g 2" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17"; BenchmarkSeconds = 120; ExtraArgs="-X 256 -g 2" } # with build
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "xevan"; BenchmarkSeconds = 120; ExtraArgs="-X 256 -g 2" } # with build
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16s"; ExtraArgs="-X 256 -g 2" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17"; ExtraArgs="-X 256 -g 2" } # with build
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "xevan"; ExtraArgs="-X 256 -g 2" } # with build
 )})
 
 if (!$Cfg.Enabled) { return }
@@ -37,7 +38,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "sgminer"
-					URI = "https://github.com/KL0nLutiy/sgminer-kl/releases/download/kl-1.0.3/sgminer-kl-1.0.3-windows.zip"
+					URI = "https://github.com/KL0nLutiy/sgminer-kl/releases/download/kl-1.0.4/sgminer-kl-1.0.4-windows.zip"
 					Path = "$Name\sgminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-k $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-listen --gpu-platform $([Config]::AMDPlatformId) $extrargs"
