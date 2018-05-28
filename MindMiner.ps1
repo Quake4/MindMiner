2b755276ac19962522c356dfd4b141cd4424498c
Quake4 <batsov@yandex.ru>
2018-05-28 23:05:52 +0300

    disable nist5 it go to asic

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -117,7 +117,7 @@ while ($true)
 			"vit" = "Vitalium"
 		})
 		# disable asic algorithms
-		$AllAlgos.Add("Disabled", @("sha256", "scrypt", "x11", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "blake"))
+		$AllAlgos.Add("Disabled", @("sha256", "scrypt", "x11", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "blake", "nist5"))
 
 		Write-Host "Pool(s) request ..." -ForegroundColor Green
 		$AllPools = Get-PoolInfo ([Config]::PoolsLocation)
