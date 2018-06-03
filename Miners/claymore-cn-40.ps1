c6328ef9a21bf4cc124158ee85c15e61e6cbf54d
unknown <batsov@yandex.ru>
2018-06-03 16:41:41 +0300

    added claymore cryptonight cpu miner

---
+++ Miners/claymore-cn-40.ps1
@@ -0,0 +1,60 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $false
+	BenchmarkSeconds = 60
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "cryptonight" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv7"; ExtraArgs = "-pow7 1" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$file = [IO.Path]::Combine($BinLocation, $Name, "epools.txt")
+if ([IO.File]::Exists($file)) {
+	[IO.File]::Delete($file)
+}
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$pool_url = "$($Pool.Protocol)://$($Pool.Host):$($Pool.Port)"
+				if ($Algo -contains "cryptonightv7") {
+					$pool_url = "$($Pool.Host):$($Pool.PortUnsecure)"
+				}
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::CPU
+					API = "claymore"
+					URI = "http://mindminer.online/miners/AMD/claymore/Claymore-CryptoNote-CPU-Miner-v4.0.zip"
+					Path = "$Name\NsCpuCNMiner64.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-o $pool_url -u $($Pool.User) -p $($Pool.Password) -retrydelay $($Config.CheckTimeout) -r -1 -mport 3335 $extrargs"
+					Port = 3335
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+				}
+			}
+		}
+	}
+}
