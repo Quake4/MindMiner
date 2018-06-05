cccbc55004b6743f4a0e3fd5c6405f5427e0eb9c
Samsonov <samsonov@ckrt.ru>
2018-06-05 09:26:51 +0300

    less request to exchange if balance not showing

--- Code/Get-RateInfo.ps1
+++ Code/Get-RateInfo.ps1
@@ -1,26 +1,30 @@
 function Get-RateInfo {
 	$result = [Collections.Generic.Dictionary[string, object]]::new()
 
-	"BTC", ($Config.Wallet | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name) | ForEach-Object {
+	$conins = [Collections.ArrayList]::new()
+	$conins.Add("BTC");
+	$conins.AddRange(($Config.Wallet | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name))
+	$conins | ForEach-Object {
 		$wallet = "$_"
-		if (!$result.ContainsKey($wallet)) {
+		# only BTC if show balance is off
+		if (!$result.ContainsKey($wallet) -and ($wallet.Contains("BTC") -or $Config.ShowBalance -eq $true)) {
 			$json = Get-UrlAsJson "https://api.coinbase.com/v2/exchange-rates?currency=$wallet"
-			$values = [Collections.Generic.List[object]]::new()
-			$Config.Currencies | ForEach-Object {
-				if ([string]::Equals($_[0], $wallet, [StringComparison]::InvariantCultureIgnoreCase)) {
-					$values.Add(@($wallet, [decimal]1))
+			if ($json) {
+				$values = [Collections.Generic.List[object]]::new()
+				$Config.Currencies | ForEach-Object {
+					if ([string]::Equals($_[0], $wallet, [StringComparison]::InvariantCultureIgnoreCase)) {
+						$values.Add(@($wallet, [decimal]1))
+					}
+					elseif ([string]::Equals($_[0], "m$wallet", [StringComparison]::InvariantCultureIgnoreCase)) {
+						$values.Add(@("m$wallet", [decimal]1000))
+					}
+					elseif ($json.data.rates."$($_[0])") {
+						$values.Add(@($_[0], [decimal]$json.data.rates."$($_[0])"))
+					}
 				}
-				elseif ([string]::Equals($_[0], "m$wallet", [StringComparison]::InvariantCultureIgnoreCase)) {
-					$values.Add(@("m$wallet", [decimal]1000))
-				}
-				elseif ($json.data.rates."$($_[0])") {
-					$values.Add(@($_[0], [decimal]$json.data.rates."$($_[0])"))
-				}
+				$result.Add($wallet, $values)
 			}
-			$result.Add($wallet, $values)
-			Remove-Variable values
 		}
-		Remove-Variable wallet
 	}
 	,$result
 }
