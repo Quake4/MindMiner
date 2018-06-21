b1a93bfcaadcd0fa370d73d6f1177c3fa659072f
unknown <batsov@yandex.ru>
2018-06-21 23:38:03 +0300

    fix error with NH wallet

--- Pools/NiceHash.ps1
+++ Pools/NiceHash.ps1
@@ -21,7 +21,7 @@ $Cfg = ReadOrCreateConfig "Do you want to mine on $($PoolInfo.Name) (>0.1 BTC ev
 if (!$Cfg) { return $null }
 
 $Wallet = if ($Config.Wallet.NiceHash) { $Config.Wallet.NiceHash } else { $Config.Wallet.BTC }
-$Sign = "BTC"
+$Sign = if ($Config.Wallet.NiceHash) { "NiceHash" } else { "BTC" } 
 if (![string]::IsNullOrWhiteSpace($Cfg.Wallet)) {
 	if (!$Config.Wallet.NiceHash) { $Config.Wallet | Add-Member NiceHash $Cfg.Wallet } else { $Config.Wallet.NiceHash = $Cfg.Wallet }
 	$Sign = "NiceHash"
