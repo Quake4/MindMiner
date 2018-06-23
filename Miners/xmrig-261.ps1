a757728409ce9b21fd3d61ad6de349bf90c8eb3a
unknown <batsov@yandex.ru>
2018-06-23 16:04:11 +0300

    xmrig from amd & nvidia

---
+++ Miners/xmrig-261.ps1
@@ -0,0 +1,77 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 90
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv7"; ExtraArgs = "-a cryptonight" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptolight"; ExtraArgs = "-a cryptonight-lite" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$file = [IO.Path]::Combine($BinLocation, $Name, "config.json")
+if ([IO.File]::Exists($file)) {
+	[IO.File]::Delete($file)
+}
+
+$urlamd = if ([Config]::Is64Bit -eq $true) { "https://github.com/xmrig/xmrig-amd/releases/download/v2.6.1/xmrig-amd-2.6.1-win64.zip" } else { "https://github.com/xmrig/xmrig-amd/releases/download/v2.6.1/xmrig-amd-2.6.1-win32.zip" }
+$urlnvidia = "https://github.com/xmrig/xmrig-nvidia/releases/download/v2.6.1/xmrig-nvidia-2.6.1-cuda9-win64.zip"
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "xmrig"
+					URI = $urlamd
+					Path = "$Name\xmrig-amd.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-port=4044 --variant 1 --donate-level=1 --opencl-platform=$([Config]::AMDPlatformId) -R $($Config.CheckTimeout) $extrargs"
+					Port = 4044
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 1
+				}
+				if ([Config]::Is64Bit -eq $true) {
+					[MinerInfo]@{
+						Pool = $Pool.PoolName()
+						PoolKey = $Pool.PoolKey()
+						Name = $Name
+						Algorithm = $Algo
+						Type = [eMinerType]::nVidia
+						API = "xmrig"
+						URI = $urlnvidia
+						Path = "$Name\xmrig-nvidia.exe"
+						ExtraArgs = $extrargs
+						Arguments = "-o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-port=4043 --variant 1 --donate-level=1 -R $($Config.CheckTimeout) $extrargs"
+						Port = 4043
+						BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+						RunBefore = $_.RunBefore
+						RunAfter = $_.RunAfter
+						Fee = 1
+					}
+				}
+			}
+		}
+	}
+}
