a8a0ee0188bd184539a019738bf924333442874e
Samsonov <samsonov@ckrt.ru>
2018-06-25 08:48:16 +0300

    version 2.44

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -52,7 +52,7 @@ class Config : BaseConfig {
 	static [int] $Processors = 0
 	static [int] $Cores = 0
 	static [int] $Threads = 0
-	static [string] $Version = "v2.43"
+	static [string] $Version = "v2.44"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
