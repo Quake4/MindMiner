166d1b61f8434794ef614ccd7933aaa90610079d
Samsonov <samsonov@ckrt.ru>
2018-07-02 08:40:58 +0300

    ccminer allium only for 32bit

--- Miners/ccminer-allium-230.ps1
+++ Miners/ccminer-allium-230.ps1
@@ -7,6 +7,7 @@ License GPL-3.0
 . .\Code\Include.ps1
 
 if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
+if ([Config]::Is64Bit) { exit }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
