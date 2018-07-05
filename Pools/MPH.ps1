80c0d8af058fe3c2914ce8f9a2b6aaa425402213
Samsonov <samsonov@ckrt.ru>
2018-07-05 11:48:45 +0300

    exclude no autoexcahge coins in balance

--- Pools/MPH.ps1
+++ Pools/MPH.ps1
@@ -47,7 +47,7 @@ $PoolInfo.AnswerTime = [DateTime]::Now
 if ($RequestBalance) {
 	$RequestBalance.getuserallbalances.data | ForEach-Object {
 		$sign = if ($_.coin -eq "bitcoin") { "BTC" } else { (Get-Culture).TextInfo.ToTitleCase($_.coin) }
-		if ($sign -eq "BTC" -or $_.confirmed -gt 0 -or $_.unconfirmed -gt 0) {
+		if (($sign -eq "BTC" -or $_.confirmed -gt 0 -or $_.unconfirmed -gt 0) -and $NoExchangeCoins -notcontains $_.coin) {
 			$PoolInfo.Balance.Add($sign, [BalanceInfo]::new([decimal]($_.confirmed), [decimal]($_.unconfirmed)))
 		}
 	}
