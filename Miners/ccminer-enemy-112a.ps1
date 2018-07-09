394aec149e7127f219b25b13db038f1926366d62
unknown <batsov@yandex.ru>
2018-07-09 19:39:14 +0300

    update ccminer enemy to 1.12a

--- Miners/ccminer-enemy-112a.ps1
+++ Miners/ccminer-enemy-112a.ps1
@@ -50,7 +50,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = if ($Algo -match "x16.") { "ccminer_woe" } else { "ccminer" }
-					URI = "ftp://radio.r41.ru/z-enemy.1-12public.zip"
+					URI = "http://mindminer.online/miners/nVidia/z-enemy.112a.zip"
 					Path = "$Name\z-enemy.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) $N $extrargs"
