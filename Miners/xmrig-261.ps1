ebca18f9f240e4119339abb91b358dcc132fbcad
unknown <batsov@yandex.ru>
2018-07-09 19:57:17 +0300

    extend default bench time in xmrig #109

--- Miners/xmrig-261.ps1
+++ Miners/xmrig-261.ps1
@@ -12,7 +12,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
 	Enabled = $true
-	BenchmarkSeconds = 90
+	BenchmarkSeconds = 120
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv7"; ExtraArgs = "-a cryptonight" }
