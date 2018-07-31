a89f9a3a3cc6534c810e332eadd13b3b776e5ee2
Samsonov <samsonov@ckrt.ru>
2018-07-31 10:30:06 +0300

    missing header

--- Code/Get-RateInfo.ps1
+++ Code/Get-RateInfo.ps1
@@ -1,3 +1,9 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
 function Get-RateInfo {
 	$result = [Collections.Generic.Dictionary[string, object]]::new()
 
