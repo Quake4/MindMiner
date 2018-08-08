3b0df51e28c599eb4620e6751438639042ddc8ea
Samsonov <samsonov@ckrt.ru>
2018-08-08 08:57:05 +0300

    update cryptodredge to 0.8.3 #146 #148

--- Miners/ccminer-dredge-083.ps1
+++ Miners/ccminer-dredge-083.ps1
@@ -44,7 +44,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "dredge"
-					URI = "https://github.com/technobyl/CryptoDredge/releases/download/v0.8.2/CryptoDredge_0.8.2_cuda_9.2_windows.zip"
+					URI = "https://github.com/technobyl/CryptoDredge/releases/download/v0.8.3/CryptoDredge_0.8.3_cuda_9.2_windows.zip"
 					Path = "$Name\cryptodredge.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --retry-pause $($Config.CheckTimeout) -b 127.0.0.1:4068 --api-type ccminer-tcp --no-watchdog $N $extrargs"
