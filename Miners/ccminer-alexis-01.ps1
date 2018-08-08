1404d61e4e95f2e9c8515cdacd4b362761d66fc9
Samsonov <samsonov@ckrt.ru>
2018-08-08 09:39:47 +0300

    disable some algo in alexis

--- Miners/ccminer-alexis-01.ps1
+++ Miners/ccminer-alexis-01.ps1
@@ -24,10 +24,10 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "nist5" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sib" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sib"; ExtraArgs = "-i 21" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "skein" }
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "skein" } # klaust faster
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "c11" } # enemy faster
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "c11"; ExtraArgs = "-i 21" } # enemy faster
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17"; BenchmarkSeconds = 120 }
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "x17"; BenchmarkSeconds = 120 } # enemy faster
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "veltor" }
 )})
 
