5dd1e74377c0538ea7dc2c7a0bda9027fe33adce
unknown <batsov@yandex.ru>
2018-08-16 01:18:52 +0300

    fix empty download miners

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -159,8 +159,8 @@ while ($true)
 			$DownloadJob.Dispose()
 			$DownloadJob = $null
 		}
-		$DownloadMiners = $AllMiners | Where-Object { !$_.Exists([Config]::BinLocation) } | Select-Object Name, Path, URI -Unique;
-		if ($DownloadMiners.Length -gt 0 -or $DownloadMiners -is [PSCustomObject]) {
+		$DownloadMiners = $AllMiners | Where-Object { !$_.Exists([Config]::BinLocation) } | Select-Object Name, Path, URI -Unique
+		if ($DownloadMiners -and ($DownloadMiners.Length -gt 0 -or $DownloadMiners -is [PSCustomObject])) {
 			Write-Host "Download miner(s): $(($DownloadMiners | Select-Object Name -Unique | ForEach-Object { $_.Name }) -Join `", `") ... " -ForegroundColor Green
 			if (!$DownloadJob) {
 				$PathUri = $DownloadMiners | Select-Object Path, URI -Unique;
@@ -400,7 +400,7 @@ while ($true)
 		Out-PoolBalance ($verbose -eq [eVerbose]::Minimal)
 	}
 	Out-Footer
-	if ($DownloadMiners.Length -gt 0 -or $DownloadMiners -is [PSCustomObject]) {
+	if ($DownloadMiners -and ($DownloadMiners.Length -gt 0 -or $DownloadMiners -is [PSCustomObject])) {
 		Write-Host "Download miner(s): $(($DownloadMiners | Select-Object Name -Unique | ForEach-Object { $_.Name }) -Join `", `") ... " -ForegroundColor Yellow
 	}
 	if ($global:HasConfirm) {
