00fb6880112795c0b10ba954a07112c9241933c7
unknown <batsov@yandex.ru>
2018-08-19 13:07:36 +0300

    version 2.99

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -59,7 +59,7 @@ class Config : BaseConfig {
 	static [int] $Processors = 0
 	static [int] $Cores = 0
 	static [int] $Threads = 0
-	static [string] $Version = "v2.98"
+	static [string] $Version = "v2.99"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
