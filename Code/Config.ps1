5406f2541392b6e524e7c06698d58faec2aef3a4
Samsonov <samsonov@ckrt.ru>
2018-09-11 14:53:40 +0300

    version 2.114

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -59,7 +59,7 @@ class Config : BaseConfig {
 	static [int] $Processors = 0
 	static [int] $Cores = 0
 	static [int] $Threads = 0
-	static [string] $Version = "v2.113"
+	static [string] $Version = "v2.114"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
