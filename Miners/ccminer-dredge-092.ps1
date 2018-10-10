7b3120786b3e0e06c5ea52d326beeb88c170d7d1
unknown <batsov@yandex.ru>
2018-10-10 15:07:07 +0300

    update dredge to 0.9.2 #206

--- Miners/ccminer-dredge-092.ps1
+++ Miners/ccminer-dredge-092.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
@@ -18,6 +18,7 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "blake2s" } # only dual
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptolightv7" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightfast" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonighthaven" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightheavy" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv7" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lbk3" }
@@ -40,26 +41,24 @@ $Cfg.Algorithms | ForEach-Object {
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
 			if ($Pool) {
-				if (!($Pool.Name -contains "nicehash" -and $Algo -like "crypto?ight*")) {
-					$N = Get-CCMinerStatsAvg $Algo $_
-					$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
-					[MinerInfo]@{
-						Pool = $Pool.PoolName()
-						PoolKey = $Pool.PoolKey()
-						Name = $Name
-						Algorithm = $Algo
-						Type = [eMinerType]::nVidia
-						API = "dredge"
-						URI = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.0/CryptoDredge_0.9.0_cuda_9.2_windows.zip"
-						Path = "$Name\cryptodredge.exe"
-						ExtraArgs = $extrargs
-						Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --retry-pause $($Config.CheckTimeout) -b 127.0.0.1:4068 --api-type ccminer-tcp --no-watchdog $N $extrargs"
-						Port = 4068
-						BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
-						RunBefore = $_.RunBefore
-						RunAfter = $_.RunAfter
-						Fee = 1
-					}
+				$N = Get-CCMinerStatsAvg $Algo $_
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					API = "dredge"
+					URI = "https://github.com/technobyl/CryptoDredge/releases/download/v0.9.2/CryptoDredge_0.9.2_cuda_9.2_windows.zip"
+					Path = "$Name\cryptodredge.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --retry-pause $($Config.CheckTimeout) -b 127.0.0.1:4068 --api-type ccminer-tcp --no-watchdog $N $extrargs"
+					Port = 4068
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 1
 				}
 			}
 		}
