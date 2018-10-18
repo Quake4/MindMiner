97988243fc7e33287779fbfdc9941fc96946f129
Samsonov <samsonov@ckrt.ru>
2018-10-18 13:57:53 +0300

    update monero to v8 fork

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -99,7 +99,7 @@ while ($true)
 			"blake256r8" = "Blake"
 			"cryptonight_heavy" = "Cryptonightheavy"
 			"cryptonight_lite_v7" = "Cryptolightv7"
-			"cryptonight-monero" = "Cryptonightv7"
+			"cryptonight-monero" = "Cryptonightv8"
 			"cryptonight_v7" = "Cryptonightv7"
 			"cryptonight_v8" = "Cryptonightv8"
 			"daggerhashimoto" = "Ethash"
