2dae499aa192c9eef255ebbd671de344cd52a2a7
Samsonov <samsonov@ckrt.ru>
2018-10-19 09:16:55 +0300

    disable tribus it go to fpga

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -129,7 +129,7 @@ while ($true)
 			"vit" = "Vitalium"
 		})
 		# disable asic algorithms
-		$AllAlgos.Add("Disabled", @("sha256", "scrypt", "x11", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "blake", "nist5", "cryptonight", "x11gost", "groestl", "equihash"))
+		$AllAlgos.Add("Disabled", @("sha256", "scrypt", "x11", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "blake", "nist5", "cryptonight", "x11gost", "groestl", "equihash", "tribus"))
 
 		# ask needed pools
 		if ($global:AskPools -eq $true) {
