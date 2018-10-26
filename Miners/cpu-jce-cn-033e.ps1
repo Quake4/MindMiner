5253154bc2391f2214bd498148d67ab7d35ee491
Oleg Samsonov <batsov@yandex.ru>
2018-10-26 17:37:02 +0300

    update jce cpu miner to 0.33e #249

--- Miners/cpu-jce-cn-033e.ps1
+++ Miners/cpu-jce-cn-033e.ps1
@@ -40,7 +40,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "jce"
-					URI = "https://github.com/jceminer/cn_cpu_miner/raw/master/jce_cn_cpu_miner.windows.033d.zip"
+					URI = "https://github.com/jceminer/cn_cpu_miner/raw/master/jce_cn_cpu_miner.windows.033e.zip"
 					Path = "$Name\$file"
 					ExtraArgs = $extrargs
 					Arguments = "-o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --low --forever --any --mport 4046 $extrargs"
