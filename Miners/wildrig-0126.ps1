76e68e056ebc0c1b3e653fad063828ca24d44b3b
Samsonov <samsonov@ckrt.ru>
2018-10-31 10:08:32 +0300

    update wildrig to 0.12.6

---
+++ Miners/wildrig-0126.ps1
@@ -0,0 +1,94 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 90
+	ExtraArgs = $null
+	Algorithms = @(
+		# [AlgoInfoEx]@{ Enabled = $true; Algorithm = "aergo"; ExtraArgs="--opencl-launch 19x128" } # invalid share on zpool
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bcd" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bitcore" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "c11" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "exosis" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "geek" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hex" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hmq1725" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "phi" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "renesis" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "skunkhash" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sonoa" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "timetravel" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "tribus" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16s" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x22i" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				$add = [string]::Empty
+				if ($extrargs -notmatch "--opencl-threads") {
+					switch ($_.Algorithm) {
+						"x22i" { $add = Get-Join " " @($add, "--opencl-threads 2") }
+						default { $add = Get-Join " " @($add, "--opencl-threads 3") }
+					}
+				}
+				if ($extrargs -notmatch "--opencl-launch") {
+					$opencl = [string]::Empty
+					switch ($_.Algorithm) {
+						"c11" { $opencl = "17x128" }
+						"hex" { $opencl = "21x128" }
+						"phi" { $opencl = "18x128" }
+						"renesis" { $opencl = "21x128" }
+						"skunkhash" { $opencl = "19x0" }
+						"sonoa" { $opencl = "19x0" }
+						"timetravel" { $opencl = "20x128" }
+						"tribus" { $opencl = "21x0" }
+						"x16r" { $opencl = "20x128" }
+						"x16s" { $opencl = "20x128" }
+						"x17" { $opencl = "20x0" }
+						"x22i" { $opencl = "19x0" }
+						default { $opencl = "19x128" }
+					}
+					$add = Get-Join " " @($add, "--opencl-launch", $opencl)
+					Remove-Variable opencl
+				}
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "xmrig"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.12.6/wildrig-multi-0.12.6-beta.7z"
+					Path = "$Name\wildrig.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --api-port=4028 $add $extrargs"
+					Port = 4028
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 2
+				}
+			}
+		}
+	}
+}
