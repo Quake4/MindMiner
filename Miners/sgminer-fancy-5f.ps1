e54a1c43136aded6254f9cce12731d9df5c8a0e9
unknown <batsov@yandex.ru>
2018-11-11 13:31:38 +0300

    add sqminer fancy 5f

---
+++ Miners/sgminer-fancy-5f.ps1
@@ -0,0 +1,64 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 90
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "allium" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lyra2h"; BenchmarkSeconds = 120 } # with build
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lyra2z" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "phi2" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x22i"; BenchmarkSeconds = 180 } # with build
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				$add = [string]::Empty
+				if ($extrargs -notmatch "-w ") {
+					$add = Get-Join " " @($add, "-w 256")
+				}
+				if ($extrargs -notmatch "-I ") {
+					$add = Get-Join " " @($add, "-I 23")
+				}
+				if ($extrargs -notmatch "-g " -and ($_.Algorithm -eq "x22i" -or $_.Algorithm -eq "allium")) {
+					$add = Get-Join " " @($add, "-g 2")
+				}
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "sgminer"
+					URI = "https://github.com/fancyIX/sgminer-phi2-branch/releases/download/5.6.1.3.b5f%2B/sgminer-phi2-fancyIX-win64-beta5f+.zip"
+					Path = "$Name\sgminer.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-k $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api-listen --gpu-platform $([Config]::AMDPlatformId) $add $extrargs"
+					Port = 4028
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 1
+				}
+			}
+		}
+	}
+}
