f607ae3f045e2a9a7b54df51f73faf53b0cbb00c
Samsonov <samsonov@ckrt.ru>
2018-11-20 14:54:37 +0300

    update t-rex to 0.8.0

--- Miners/ccminer-trex-80.ps1
+++ Miners/ccminer-trex-80.ps1
@@ -18,6 +18,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bcd" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bitcore" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "c11" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "geek" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hmq1725" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hsr" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "lyra2z" } # dredge faster
@@ -38,9 +39,9 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 if (!$Cfg.Enabled) { return }
 
 switch ([Config]::CudaVersion) {
-	([version]::new(10, 0)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.7.4/t-rex-0.7.4-win-cuda10.0.zip" }
-	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.7.4/t-rex-0.7.4-win-cuda9.2.zip" }
-	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.7.4/t-rex-0.7.4-win-cuda9.1.zip" }
+	([version]::new(10, 0)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.0/t-rex-0.8.0-win-cuda10.0.zip" }
+	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.0/t-rex-0.8.0-win-cuda9.2.zip" }
+	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.0/t-rex-0.8.0-win-cuda9.1.zip" }
 }
 
 $Cfg.Algorithms | ForEach-Object {
