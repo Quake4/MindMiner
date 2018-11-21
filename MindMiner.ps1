a58487a3154c80ad4e291d9d00a27b349f51c045
Samsonov <samsonov@ckrt.ru>
2018-11-21 09:52:08 +0300

    move devices up

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -401,11 +401,11 @@ while ($true)
 
 	$verbose = $Config.Verbose -as [eVerbose]
 
+	Out-DeviceInfo ($verbose -eq [eVerbose]::Minimal)
+
 	if ($verbose -eq [eVerbose]::Full) {
 		Out-PoolInfo
 	}
-
-	Out-DeviceInfo ($verbose -eq [eVerbose]::Minimal)
 	
 	$mult = if ($verbose -eq [eVerbose]::Normal) { 0.65 } else { 0.80 }
 	$alg = [hashtable]::new()
