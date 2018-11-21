490ff68c4534b47133ce79f489edae68db8416e8
Samsonov <samsonov@ckrt.ru>
2018-11-21 13:09:09 +0300

    exclude ld algos from zpool

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -137,7 +137,7 @@ while ($true)
 			"vit" = "Vitalium"
 		})
 		# disable asic algorithms
-		$AllAlgos.Add("Disabled", @("sha256", "scrypt", "scrypt-ld", "x11", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "sia", "blake", "nist5", "cryptonight", "x11gost", "groestl", "equihash", "tribus"))
+		$AllAlgos.Add("Disabled", @("sha256", "sha256-ld", "scrypt", "scrypt-ld", "x11", "x11-ld", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "sia", "blake", "nist5", "cryptonight", "x11gost", "groestl", "equihash", "tribus"))
 
 		# ask needed pools
 		if ($global:AskPools -eq $true) {
