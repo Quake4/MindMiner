94f296e011477ad82bd6809428897fc86def7fe9
unknown <batsov@yandex.ru>
2018-11-22 20:37:34 +0300

    rename message

--- Code/Get-DeviceInfo.ps1
+++ Code/Get-DeviceInfo.ps1
@@ -92,7 +92,7 @@ function Get-SMIInfo ([Parameter(Mandatory)][string] $arg) {
 }
 
 function Get-Devices ([Parameter(Mandatory)] [eMinerType[]] $types, $olddevices) {
-	Write-Host "Getting devices status ..." -ForegroundColor Green
+	Write-Host "Retrieve devices status ..." -ForegroundColor Green
 	$result = [Collections.Generic.Dictionary[eMinerType, Collections.Generic.List[DeviceInfo]]]::new()
 	$types | ForEach-Object {
 		$type = $_
