7ea459e2b66387eff89d1ce6482cf1528c627b15
unknown <batsov@yandex.ru>
2018-11-26 21:58:22 +0300

    calcpower

--- Code/DeviceInfo.ps1
+++ Code/DeviceInfo.ps1
@@ -25,4 +25,20 @@ class GPUInfo : DeviceInfo {
 	[decimal] $PowerLimit
 	[decimal] $Clock
 	[decimal] $ClockMem
+
+	[void] CalcPower() {
+		[int] $pwr = 0
+		switch ($this.Name) {
+			"Vega 64" { $pwr = 230 }
+			"Vega 56" { $pwr = 210 }
+			"RX 580" { $pwr = 135 }
+			"RX 480" { $pwr = 135 }
+			"RX 570" { $pwr = 120 }
+			"RX 470" { $pwr = 120 }
+			"RX 560" { $pwr = 75 }
+			"RX 460" { $pwr = 75 }
+			Default {}
+		}
+		$this.Power = $this.PowerLimit * $pwr * $this.Load / 100;
+	}
 }
