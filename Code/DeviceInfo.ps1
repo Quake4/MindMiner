1f58970b45100f1fa2e284a7c7aa114808660cec
unknown <batsov@yandex.ru>
2018-11-26 23:14:33 +0300

    update power from specs amd.com

--- Code/DeviceInfo.ps1
+++ Code/DeviceInfo.ps1
@@ -29,14 +29,16 @@ class GPUInfo : DeviceInfo {
 	[void] CalcPower() {
 		[int] $pwr = 0
 		switch ($this.Name) {
-			"Vega 64" { $pwr = 230 }
+			"Vega 64" { $pwr = 295 }
 			"Vega 56" { $pwr = 210 }
-			"RX 580" { $pwr = 135 }
-			"RX 480" { $pwr = 135 }
-			"RX 570" { $pwr = 120 }
+			"RX 590" { $pwr = 225 }
+			"RX 580" { $pwr = 185 }
+			"RX 480" { $pwr = 185 }
+			"RX 570" { $pwr = 150 }
 			"RX 470" { $pwr = 120 }
-			"RX 560" { $pwr = 75 }
-			"RX 460" { $pwr = 75 }
+			"RX 560" { $pwr = 80 }
+			"RX 460" { $pwr = 70 }
+			"RX 550" { $pwr = 50 }
 			Default {}
 		}
 		$this.Power = $this.PowerLimit * $pwr * $this.Load / 10000;
