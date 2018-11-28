b2fdec45043339f54f81a9ed9f5f0af2f70ebcdf
platinum4 <platinumstephen@gmail.com>
2018-11-27 23:46:03 -0600

    Update ccminer-yescrypt-8.ps1

--- Miners/ccminer-yescrypt-8.ps1
+++ Miners/ccminer-yescrypt-8.ps1
@@ -12,7 +12,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
 	Enabled = $true
 	BenchmarkSeconds = 300
-	ExtraArgs = null
+	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "yescrypt" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "yescryptr8" }
