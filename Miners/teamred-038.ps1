fa1981b89335fa1baa575dd37cec09b47e4434d9
Samsonov <samsonov@ckrt.ru>
2018-11-28 15:07:12 +0300

    update teamred miner to 0.3.8

--- Miners/teamred-038.ps1
+++ Miners/teamred-038.ps1
@@ -28,6 +28,8 @@ $Cfg.Algorithms | ForEach-Object {
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
 			if ($Pool) {
+				$fee = 3
+				if ($_.Algorithm -match "cnv8") { $fee = 2.5}
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
 				[MinerInfo]@{
 					Pool = $Pool.PoolName()
@@ -36,7 +38,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "teamred"
-					URI = "https://github.com/todxx/teamredminer/releases/download/v0.3.7/teamredminer-v0.3.7-win.zip"
+					URI = "https://github.com/todxx/teamredminer/releases/download/v0.3.8/teamredminer-v0.3.8-win.zip"
 					Path = "$Name\teamredminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api_listen=127.0.0.1:4028 --platform=$([Config]::AMDPlatformId) $extrargs"
@@ -44,7 +46,7 @@ $Cfg.Algorithms | ForEach-Object {
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
 					RunAfter = $_.RunAfter
-					Fee = 3
+					Fee = $fee
 				}
 			}
 		}
