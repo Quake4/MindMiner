a0e79d5dd43640b41969460f25dcf49cf0472266
Samsonov <samsonov@ckrt.ru>
2018-11-28 10:02:59 +0300

    yescrypt32 = 1 stats avg

--- Code/Include.ps1
+++ Code/Include.ps1
@@ -142,7 +142,7 @@ function ReadOrCreateMinerConfig(
 	}
 }
 
-[hashtable] $CCMinerStatsAvg = @{ "Phi" = 1; "Tribus" = 1; "Lyra2re2" = 1; "Lyra2z" = 1; "X17" = 1; "Xevan" = 1 }
+[hashtable] $CCMinerStatsAvg = @{ "Phi" = 1; "Tribus" = 1; "Lyra2re2" = 1; "Lyra2z" = 1; "X17" = 1; "Xevan" = 1; "Yescryptr32" = 1 }
 
 function Get-CCMinerStatsAvg (
 	[Parameter(Mandatory)] [string] $algo, # Get-Algo
--- Miners/ccminer-yescrypt-8.ps1
+++ Miners/ccminer-yescrypt-8.ps1
@@ -12,7 +12,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
 	Enabled = $true
 	BenchmarkSeconds = 120
-	ExtraArgs = "-i 12.5"
+	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "yescrypt" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "yescryptr8" }
