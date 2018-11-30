85230a99510d5dc8f8fc5370690e2a444412c827
platinum4 <platinumstephen@gmail.com>
2018-11-30 04:16:05 -0600

    Update and rename cpu-jce-cn-033f.ps1 to cpu-jce-cn-033g.ps1

--- Miners/cpu-jce-cn-033g.ps1
+++ Miners/cpu-jce-cn-033g.ps1
@@ -14,7 +14,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "cryptonight" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv7" }
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "cryptonightv7" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightheavy" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonightv8" }
 )}
@@ -49,7 +49,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "jce"
-					URI = "https://github.com/jceminer/cn_cpu_miner/raw/master/jce_cn_cpu_miner.windows.033f.zip"
+					URI = "https://github.com/jceminer/cn_cpu_miner/raw/master/jce_cn_cpu_miner.windows.033g.zip"
 					Path = "$Name\$file"
 					ExtraArgs = $extrargs
 					Arguments = "-o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --low --forever --any --mport 4046 $add $extrargs"
