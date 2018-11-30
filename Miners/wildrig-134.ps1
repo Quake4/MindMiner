d097956bb17f0065feee3110df48da901605fefc
unknown <batsov@yandex.ru>
2018-11-30 20:14:41 +0300

    update wildrig to 0.13.4

--- Miners/wildrig-134.ps1
+++ Miners/wildrig-134.ps1
@@ -18,6 +18,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bcd" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bitcore" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "c11" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "dedal" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "exosis" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "geek" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hex" }
@@ -32,6 +33,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16s" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x18" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x21s" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x22i" }
 )}
 
@@ -54,19 +56,19 @@ $Cfg.Algorithms | ForEach-Object {
 					$opencl = [string]::Empty
 					switch ($_.Algorithm) {
 						"aergo" { $opencl = "17x0" }
-						"bcd" { $opencl = "20x0" }
+						"bitcore" { $opencl = "19x0" }
+						"c11" { $opencl = "19x0" }
 						"geek" { $opencl = "20x128" }
 						"hex" { $opencl = "22x0" }
-						"hmq1725" { $opencl = "21x128" }
-						"renesis" { $opencl = "20x128" }
-						"timetravel" { $opencl = "19x128" }
+						"hmq1725" { $opencl = "21x0" }
+						"phi" { $opencl = "19x0" }
+						"renesis" { $opencl = "18x128" }
+						"skunkhash" { $opencl = "18x0" }
+						"sonoa" { $opencl = "18x0" }
+						"timetravel" { $opencl = "17x128" }
 						"tribus" { $opencl = "21x0" }
-						"x16r" { $opencl = "18x0" }
-						"x16s" { $opencl = "18x0" }
-						"x17" { $opencl = "18x0" }
-						"x18" { $opencl = "18x0" }
 						"x22i" { $opencl = "18x0" }
-						default { $opencl = "19x0" }
+						default { $opencl = "20x0" }
 					}
 					$add = Get-Join " " @($add, "--opencl-launch", $opencl)
 					Remove-Variable opencl
@@ -78,7 +80,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.13.2/wildrig-multi-0.13.2-beta.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.13.4/wildrig-multi-0.13.4-beta.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --api-port=4028 $add $extrargs"
