e814e3b238111f2d7c4a38ad4843146d773cd8de
unknown <batsov@yandex.ru>
2018-12-02 23:14:30 +0300

    add gminer 1.10

---
+++ Miners/gminer-110.ps1
@@ -0,0 +1,54 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
+	Enabled = $true
+	BenchmarkSeconds = 120
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "zhash" }
+)}
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$alg = [string]::Empty
+				if ($_.Algorithm -match "zhash") {
+					$alg = "-a 144_5 --pers auto"
+				}
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					API = "gminer"
+					URI = "http://mindminer.online/miners/nVidia/gminer-110.zip"
+					Path = "$Name\miner.exe"
+					ExtraArgs = $extrargs
+					Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api 42000 --pec 0 -w 0 $extrargs"
+					Port = 42000
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 2
+				}
+			}
+		}
+	}
+}
