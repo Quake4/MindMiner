548863a9a1934a51e669915cc4096ea6660483e0
unknown <batsov@yandex.ru>
2018-12-12 20:35:38 +0300

    update t-rex to 0.8.8

--- Miners/ccminer-trex-88.ps1
+++ Miners/ccminer-trex-88.ps1
@@ -41,9 +41,9 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 if (!$Cfg.Enabled) { return }
 
 switch ([Config]::CudaVersion) {
-	{ $_ -ge [version]::new(10, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.6/t-rex-0.8.6-win-cuda10.0.zip" }
-	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.6/t-rex-0.8.6-win-cuda9.2.zip" }
-	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.6/t-rex-0.8.6-win-cuda9.1.zip" }
+	{ $_ -ge [version]::new(10, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.8/t-rex-0.8.8-win-cuda10.0.zip" }
+	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.8/t-rex-0.8.8-win-cuda9.2.zip" }
+	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.8.8/t-rex-0.8.8-win-cuda9.1.zip" }
 }
 
 $Cfg.Algorithms | ForEach-Object {
