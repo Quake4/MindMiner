444fdc93bf4239deafd6af826efe7ad3bf19e244
Samsonov <samsonov@ckrt.ru>
2019-01-09 09:53:01 +0300

    update wildrig to 0.15.1.2 #355

--- Miners/wildrig-1512.ps1
+++ Miners/wildrig-1512.ps1
@@ -25,14 +25,18 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "hmq1725" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lyra2v3" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "lyra2vc0ban" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "mtp" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "phi" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "polytimos" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "renesis" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sha256q" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sha256t" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "skunkhash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sonoa" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "timetravel" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "tribus" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16r" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16rt" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x16s" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x17" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x18" }
@@ -54,7 +58,7 @@ $Cfg.Algorithms | ForEach-Object {
 				$add = [string]::Empty
 				if ($extrargs -notmatch "--opencl-threads") {
 					if ($_.Algorithm -eq "phi" -or $_.Algorithm -eq "renesis" -or $_.Algorithm -eq "skunkhash") { $add = Get-Join " " @($add, "--opencl-threads 3") }
-					elseif ($_.Algorithm -eq "lyra2v3") { $add = Get-Join " " @($add, "--opencl-threads 1")	}
+					elseif ($_.Algorithm -eq "lyra2v3" -or $_.Algorithm -eq "sha256q" -or $_.Algorithm -eq "sha256t") { $add = Get-Join " " @($add, "--opencl-threads 1")	}
 					else { $add = Get-Join " " @($add, "--opencl-threads 2") }
 				}
 				if ($extrargs -notmatch "--opencl-launch") {
@@ -63,12 +67,13 @@ $Cfg.Algorithms | ForEach-Object {
 						"aergo" { $opencl = "17x0" }
 						"bitcore" { $opencl = "19x0" }
 						"c11" { $opencl = "19x0" }
-						"geek" { $opencl = "20x128" }
 						"hex" { $opencl = "22x0" }
 						"hmq1725" { $opencl = "20x128" }
 						"lyra2v3" { $opencl = "23x0" }
 						"phi" { $opencl = "19x0" }
 						"renesis" { $opencl = "18x128" }
+						"sha256q" { $opencl = "29x0" }
+						"sha256t" { $opencl = "29x0" }
 						"skunkhash" { $opencl = "18x0" }
 						"sonoa" { $opencl = "18x0" }
 						"timetravel" { $opencl = "17x128" }
@@ -86,7 +91,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.14.0/wildrig-multi-0.14.0-beta.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.15.1/wildrig-multi-windows-0.15.1.2-beta.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --api-port=4028 $add $extrargs"
