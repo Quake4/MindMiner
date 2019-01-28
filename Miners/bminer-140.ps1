2b70ceab26aeb7ea9905db72830d3d036a313463
Oleg Samsonov <batsov@yandex.ru>
2019-01-28 07:30:30 +0300

    update bminer to 14.0

--- Miners/bminer-140.ps1
+++ Miners/bminer-140.ps1
@@ -79,7 +79,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "bminer"
-					URI = "https://www.bminercontent.com/releases/bminer-lite-v13.1.0-8ae5450-amd64.zip"
+					URI = "https://www.bminercontent.com/releases/bminer-lite-v14.0.0-41bef22-amd64.zip"
 					Path = "$Name\bminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-uri $proto`://$($Pool.User):$($Pool.Password.Replace(",", "%2C").Replace("/", "%2F"))@$($Pool.Host):$($Pool.Port) -watchdog=false -api 127.0.0.1:1880 $pers $extrargs"
