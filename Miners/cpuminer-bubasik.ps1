63166e3eae7d98f988d10afb43b86bd5699db862
platinum4 <platinumstephen@gmail.com>
2019-02-03 00:36:18 -0600

    Add cpuminer-bubasik.ps1

---
+++ Miners/cpuminer-bubasik.ps1
@@ -0,0 +1,68 @@
+<#
+MindMiner  Copyright (C) 2017  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::CPU) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
+	Enabled = $true
+	BenchmarkSeconds = 60
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "binarium_hash_v1" }
+)}
+
+if (!$Cfg.Enabled) { return }
+
+# choose version
+$miners = [Collections.Generic.Dictionary[string, string[]]]::new()
+$miners.Add("cpuminer-sse2.exe", @("SSE2"))
+$miners.Add("cpuminer-aes-avx.exe", @("AES", "AVX"))
+
+$bestminer = $null
+$miners.GetEnumerator() | ForEach-Object {
+	$has = $true
+	$_.Value | ForEach-Object {
+		if (![Config]::CPUFeatures.Contains($_)) {
+			$has = $false
+		}
+	}
+	if ($has) {
+		$bestminer = $_.Key
+	}
+}
+if (!$bestminer) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::CPU
+					API = "cpuminer"
+					URI = "https://github.com/bubasik/cpuminer-easy-binarium/releases/download/1.0/cpuminer-easy-binarium-win_x64.zip"
+					Path = "$Name\$bestminer"
+					ExtraArgs = $extrargs
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 4048 --cpu-priority 0 -R $($Config.CheckTimeout) $extrargs"
+					Port = 4048
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+				}
+			}
+		}
+	}
+}
