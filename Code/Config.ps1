dacf7cb84bce63667e51e0ca2e3c12b4b5d46f71
unknown <batsov@yandex.ru>
2019-02-03 22:09:39 +0300

    version 3.62

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -70,7 +70,7 @@ class Config : BaseConfig {
 	[bool] $ElectricityConsumption = $false
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v3.61"
+	static [string] $Version = "v3.62"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
