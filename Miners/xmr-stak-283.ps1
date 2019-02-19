06f8cca86c8f5cd855bfbd1ba402716fcc8bd8b9
Samsonov <samsonov@ckrt.ru>
2019-02-19 21:00:49 +0300

    update xmrstak to 2.8.3

--- Miners/xmr-stak-283.ps1
+++ Miners/xmr-stak-283.ps1
@@ -53,7 +53,7 @@ Remove-Item "$Dir\nvidia.txt" -Force -ErrorAction SilentlyContinue
 Remove-Item "$Dir\pools.txt" -Force -ErrorAction SilentlyContinue
 Save-BaseConfig $Dir
 
-$url = "https://github.com/fireice-uk/xmr-stak/releases/download/2.8.2/xmr-stak-win64-2.8.2.7z"
+$url = "https://github.com/fireice-uk/xmr-stak/releases/download/2.8.3/xmr-stak-win64-2.8.3.7z"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
