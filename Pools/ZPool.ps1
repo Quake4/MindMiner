b46f61eaf281300e8e9d30ec26a4deff839fffd2
unknown <batsov@yandex.ru>
2019-02-23 14:13:38 +0300

    fix glt again

--- Pools/ZPool.ps1
+++ Pools/ZPool.ps1
@@ -41,7 +41,7 @@ if (!$Cfg.Enabled) { return $PoolInfo }
 [decimal] $Pool_Variety = if ($Cfg.Variety) { $Cfg.Variety } else { 0.85 }
 # already accounting Aux's
 $AuxCoins = @("GLT", "UIS", "MBL")
-$GLT = @("jeonghash", "pawelhash")
+$GLT = @("astralhash", "jeonghash", "padihash", "pawelhash")
 
 try {
 	$RequestStatus = Get-UrlAsJson "https://www.zpool.ca/api/status"
