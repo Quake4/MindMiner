0c4ddaa78e97fbfaae391fd9139ada82703057d6
Samsonov <samsonov@ckrt.ru>
2019-02-25 10:42:15 +0300

    add argon2d4096 to cpuminer

--- Miners/cpuminer-opt-3881.ps1
+++ Miners/cpuminer-opt-3881.ps1
@@ -18,6 +18,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2d250" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2d500" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2d4096" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "axiom" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "blakecoin" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "blake2s" }
