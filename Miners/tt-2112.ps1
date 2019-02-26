7c6d7ecff1a5f3057abd88396043502169cf07ca
madest13 <31035328+madest13@users.noreply.github.com>
2019-02-26 09:08:23 +0300

    update tt-miner to 2.1.12
    * Update and rename tt-2111.ps1 to tt-2112.ps1
    
    fixes a bug that could cause rejected shares under certain conditions
    
    * Update tt-2112.ps1

--- Miners/tt-2112.ps1
+++ Miners/tt-2112.ps1
@@ -39,10 +39,10 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "claymore"
-					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.11.zip"
+					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.12.zip"
 					Path = "$Name\TT-Miner.exe"
 					ExtraArgs = $extrargs
-					Arguments = "-a $alg -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -worker $([Config]::WorkerNamePlaceholder) --nvidia -b 127.0.0.1:3360 -PRS 25 -PRT 6 $extrargs"
+					Arguments = "-a $alg -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -worker $([Config]::WorkerNamePlaceholder) --nvidia -b 127.0.0.1:3360 -PRS 25 -PRT 24 $extrargs"
 					Port = 3360
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
