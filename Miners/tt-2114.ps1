da2692b555c4af03efae8c023746f65e7684c598
unknown <batsov@yandex.ru>
2019-03-06 23:41:13 +0300

    disable lyra2v3 #461 in tt

--- Miners/tt-2114.ps1
+++ Miners/tt-2114.ps1
@@ -30,7 +30,7 @@ $Cfg.Algorithms | ForEach-Object {
 		if ($Algo) {
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
-			if ($Pool -and ($Pool.Name -notcontains "nicehash" -or ($Pool.Name -contains "nicehash" -and $_.Algorithm -notmatch "lyra2v3"))) {
+			if ($Pool -and $_.Algorithm -notmatch "lyra2v3") { # lyra2v3 isnt working
 				$alg = $_.Algorithm.ToUpper() + "-$([Config]::CudaVersion.Major)$([Config]::CudaVersion.Minor)"
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
 				[MinerInfo]@{
