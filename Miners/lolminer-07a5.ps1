a70a15dc31147d8925d275531ffe334f3ef7538a
Samsonov <samsonov@ckrt.ru>
2019-03-07 08:56:42 +0300

    return back ald lolminer #445

---
+++ Miners/lolminer-07a5.ps1
@@ -0,0 +1,76 @@
+<#
+MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia -and [Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 90
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beam" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				$coin = [string]::Empty
+				$fee = 2
+				if ($extrargs -notmatch "--coin ") {
+					switch ($_.Algorithm) {
+						"beam" { $coin = "--coin BEAM" }
+					}
+				}
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					TypeInKey = $true
+					API = "lolnew"
+					URI = "https://github.com/Lolliedieb/lolMiner-preview/releases/download/0.7_a5/lolMiner_v07_alpha5_Win64.zip"
+					Path = "$Name\lolMiner.exe"
+					ExtraArgs = $extrargs
+					Arguments = "$coin --pool $($Pool.Host) --port $($Pool.PortUnsecure) --user $($Pool.User) --pass $($Pool.Password) --apiport 4068 --timeprint 1 --disable_memcheck 1 --devices NVIDIA --tls 0 $extrargs"
+					Port = 4068
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = $fee
+				}
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					TypeInKey = $true
+					API = "lolnew"
+					URI = "https://github.com/Lolliedieb/lolMiner-preview/releases/download/0.7_a5/lolMiner_v07_alpha5_Win64.zip"
+					Path = "$Name\lolMiner.exe"
+					ExtraArgs = $extrargs
+					Arguments = "$coin --pool $($Pool.Host) --port $($Pool.PortUnsecure) --user $($Pool.User) --pass $($Pool.Password) --apiport 4028 --timeprint 1 --disable_memcheck 1 --devices AMD --tls 0 $extrargs"
+					Port = 4028
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = $fee
+				}
+			}
+		}
+	}
+}
