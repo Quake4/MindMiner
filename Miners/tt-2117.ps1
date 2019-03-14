ecc95cb56e2c0c9bc898bcd15c61969e7a241481
platinum4 <platinumstephen@gmail.com>
2019-03-14 06:50:30 -0500

    tt-2117

--- Miners/tt-2117.ps1
+++ Miners/tt-2117.ps1
@@ -40,7 +40,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "claymore"
-					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.16.zip"
+					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.17.zip"
 					Path = "$Name\TT-Miner.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $alg -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -worker $([Config]::WorkerNamePlaceholder) --nvidia -b 127.0.0.1:3360 -PRS 25 -PRT 24 $extrargs"
