336d0fbc2831bcf3c65ae7643b178d21b05d54cb
Samsonov <samsonov@ckrt.ru>
2019-03-15 11:53:29 +0300

    update gminer release 1.36

--- Miners/gminer-136.ps1
+++ Miners/gminer-136.ps1
@@ -72,7 +72,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey =  if ($_ -eq [eMinerType]::AMD) { $true } else { $false }
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerBetaRelease/releases/download/1.35/gminer_1_35_beta_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.36/gminer_1_36_minimal_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api $port --pec 0 -w 0 $devs $extrargs"
