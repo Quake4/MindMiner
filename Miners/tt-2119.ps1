13d69341c3424477b4b0fdc17039521ebb47523e
madest13 <31035328+madest13@users.noreply.github.com>
2019-03-18 17:07:45 +0300

    Update and rename tt-2118.ps1 to tt-2119.ps1

--- Miners/tt-2119.ps1
+++ Miners/tt-2119.ps1
@@ -40,7 +40,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "claymore"
-					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.18.zip"
+					URI = "https://tradeproject.de/download/Miner/TT-Miner-2.1.19.zip"
 					Path = "$Name\TT-Miner.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $alg -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -worker $([Config]::WorkerNamePlaceholder) --nvidia -b 127.0.0.1:3360 -PRS 25 -PRT 24 -luck $extrargs"
