a55812631946b542a30b563a730878f54d3deaf3
Samsonov <samsonov@ckrt.ru>
2019-03-22 09:06:36 +0300

    update wildrig rainforest

--- Miners/wildrig-rf-154p8.ps1
+++ Miners/wildrig-rf-154p8.ps1
@@ -42,7 +42,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.15.4p7/wildrig-multi-windows-0.15.4-preview7.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.15.4p8/wildrig-multi-windows-0.15.4-preview8.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --api-port=4028 $add $extrargs"
