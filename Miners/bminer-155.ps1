2799512c33c25aae6d8a60b2e44832d6ed325ea5
unknown <batsov@yandex.ru>
2019-04-16 09:38:30 +0300

    extend benchmark timeout in bminer

--- Miners/bminer-155.ps1
+++ Miners/bminer-155.ps1
@@ -11,7 +11,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
 	Enabled = $true
-	BenchmarkSeconds = 90
+	BenchmarkSeconds = 120
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "aeternity" }
