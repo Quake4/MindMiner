d522b89444f5df0c4cbad2247160031ccae0e079
Samsonov <samsonov@ckrt.ru>
2019-04-26 14:10:05 +0300

    updated gminer to 1.39

--- Miners/gminer-139.ps1
+++ Miners/gminer-139.ps1
@@ -74,7 +74,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey =  if ($_ -eq [eMinerType]::AMD) { $true } else { $false }
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.38/gminer_1_38_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.39/gminer_1_39_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api $port --pec 0 -w 0 $devs $extrargs"
