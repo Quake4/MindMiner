fb02d5df80736066dc9093e1f5fb793daf5570a4
Samsonov <samsonov@ckrt.ru>
2019-05-22 11:44:24 +0300

    disable tribus algo

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -170,7 +170,7 @@ while ($true)
 			"verus" = "Verushash"
 		})
 		# disable asic algorithms
-		$AllAlgos.Add("Disabled", @("argon2-crds", "argon2-dyn", "sha256", "sha256asicboost", "sha256-ld", "scrypt", "scrypt-ld", "x11", "x11-ld", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "sia", "blake", "nist5", "cryptonight", "cryptonightv7", "cryptonightv8", "cryptonightheavy", "x11gost", "groestl", "equihash", "lyra2re2", "lyra2z", "pascal", "keccak", "keccakc", "skein"))
+		$AllAlgos.Add("Disabled", @("argon2-crds", "argon2-dyn", "sha256", "sha256asicboost", "sha256-ld", "scrypt", "scrypt-ld", "x11", "x11-ld", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "sia", "blake", "nist5", "cryptonight", "cryptonightv7", "cryptonightv8", "cryptonightheavy", "x11gost", "groestl", "equihash", "lyra2re2", "lyra2z", "pascal", "keccak", "keccakc", "skein", "tribus"))
 
 		# ask needed pools
 		if ($global:AskPools -eq $true) {
