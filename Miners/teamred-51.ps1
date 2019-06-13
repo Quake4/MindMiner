81297fa4b425a56c905dba93e775caf6ebeb89ee
Samsonov <samsonov@ckrt.ru>
2019-06-13 15:10:39 +0300

    updated teamred to 0.5.1

--- Miners/teamred-51.ps1
+++ Miners/teamred-51.ps1
@@ -53,7 +53,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "teamred"
-					URI = "https://github.com/todxx/teamredminer/releases/download/0.5.0/teamredminer-v0.5.0-win.zip"
+					URI = "https://github.com/todxx/teamredminer/releases/download/0.5.1/teamredminer-v0.5.1-win.zip"
 					Path = "$Name\teamredminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) --api_listen=127.0.0.1:4028 --platform=$([Config]::AMDPlatformId) $extrargs"
