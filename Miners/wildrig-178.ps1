253687c6da71d24f4da5b33501f7f96014330d79
Samsonov <samsonov@ckrt.ru>
2019-06-20 13:20:07 +0300

    update wildrig to 0.17.8

--- Miners/wildrig-178.ps1
+++ Miners/wildrig-178.ps1
@@ -78,7 +78,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.17.7/wildrig-multi-windows-0.17.7-beta.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.17.8/wildrig-multi-windows-0.17.8-beta.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --api-port=4028 $extrargs"
