69821ab7222ad1184710670a6fb377d840508e43
unknown <batsov@yandex.ru>
2019-07-05 00:44:37 +0300

    remove comments from nlpool

--- Pools/NLPool.ps1
+++ Pools/NLPool.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2018  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018-2019  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
@@ -39,19 +39,12 @@ $PoolInfo.AverageProfit = $Cfg.AverageProfit
 if (!$Cfg.Enabled) { return $PoolInfo }
 
 [decimal] $Pool_Variety = if ($Cfg.Variety) { $Cfg.Variety } else { 0.75 }
-# already accounting Aux's
-$AuxCoins = @("UIS", "MBL")
 
 try {
 	$RequestStatus = Get-UrlAsJson "http://www.nlpool.nl/api/status"
 }
 catch { return $PoolInfo }
-<#
-try {
-	$RequestCurrency = Get-UrlAsJson "http://www.nlpool.nl/api/currencies"
-}
-catch { return $PoolInfo }
-#>
+
 try {
 	if ($Config.ShowBalance) {
 		$RequestBalance = Get-UrlAsJson "http://www.nlpool.nl/api/wallet?address=$Wallet"
@@ -59,25 +52,14 @@ try {
 }
 catch { }
 
-if (!$RequestStatus<# -or !$RequestCurrency#>) { return $PoolInfo }
+if (!$RequestStatus) { return $PoolInfo }
 $PoolInfo.HasAnswer = $true
 $PoolInfo.AnswerTime = [DateTime]::Now
 
 if ($RequestBalance) {
 	$PoolInfo.Balance.Add($Sign, [BalanceInfo]::new([decimal]($RequestBalance.balance), [decimal]($RequestBalance.unsold)))
 }
-<#
-$Currency = $RequestCurrency | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-	[PSCustomObject]@{
-		Coin = if (!$RequestCurrency.$_.symbol) { $_ } else { $RequestCurrency.$_.symbol }
-		Algo = $RequestCurrency.$_.algo
-		Profit = [decimal]$RequestCurrency.$_.estimate / 1000
-		Hashrate = $RequestCurrency.$_.hashrate 
-		Enabled = $RequestCurrency.$_.hashrate -gt 0
-		Port = $RequestCurrency.$_.port
-	}
-}
-#>
+
 $RequestStatus | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
 	$Algo = $RequestStatus.$_
 	$Pool_Algorithm = Get-Algo($Algo.name)
@@ -88,7 +70,7 @@ $RequestStatus | Get-Member -MemberType NoteProperty | Select-Object -ExpandProp
 		$Pool_Diff = if ($AllAlgos.Difficulty.$Pool_Algorithm) { "d=$($AllAlgos.Difficulty.$Pool_Algorithm)" } else { [string]::Empty }
 		$Divisor = 1000000 * $Algo.mbtc_mh_factor
 		# fix divisor for equihash144
-		if ($Algo.name -match "equihash144") { $Divisor *= 2 }
+		if ($Algo.name -match "equihash144") { $Divisor *= 1.5 }
 
 		# convert to one dimension and decimal
 		$Algo.actual_last24h = [decimal]$Algo.actual_last24h / 1000
@@ -96,37 +78,7 @@ $RequestStatus | Get-Member -MemberType NoteProperty | Select-Object -ExpandProp
 		# fix very high or low daily changes
 		if ($Algo.estimate_current -gt $Algo.actual_last24h * [Config]::MaxTrustGrow) { $Algo.estimate_current = $Algo.actual_last24h * [Config]::MaxTrustGrow }
 		if ($Algo.actual_last24h -gt $Algo.estimate_current * [Config]::MaxTrustGrow) { $Algo.actual_last24h = $Algo.estimate_current * [Config]::MaxTrustGrow }
-<#		now no dedicated ports in api
-		$CurrencyFiltered = $Currency | Where-Object { $_.Algo -eq $Algo.name -and $_.Profit -gt 0 -and $_.Enabled }
-		$CurrencyFiltered | ForEach-Object {
-			if ($_.Profit -gt $Algo.estimate_current * [Config]::MaxTrustGrow) { $_.Profit = $Algo.estimate_current * [Config]::MaxTrustGrow }
 
-			if ($Pool_Port -ne $_.Port) {
-				[decimal] $Profit = ([Math]::Min($_.Profit, $Algo.actual_last24h) + $_.Profit) / 2
-				$Profit = $Profit * (1 - [decimal]$Algo.fees / 100) * $Pool_Variety / $Divisor
-				$ProfitFast = $Profit
-				if ($Profit -gt 0) {
-					$Profit = Set-Stat -Filename $PoolInfo.Name -Key "$Pool_Algorithm`_$($_.Coin)" -Value $Profit -Interval $Cfg.AverageProfit
-				}
-
-				if ([int]$Algo.workers -ge $Config.MinimumMiners -or $global:HasConfirm) {
-					$PoolInfo.Algorithms.Add([PoolAlgorithmInfo] @{
-						Name = $PoolInfo.Name
-						Algorithm = $Pool_Algorithm
-						Profit = if (($Config.Switching -as [eSwitching]) -eq [eSwitching]::Fast) { $ProfitFast } else { $Profit }
-						Info = $_.Coin
-						InfoAsKey = $true
-						Protocol = "stratum+tcp"
-						Host = $Pool_Host
-						Port = $Pool_Port
-						PortUnsecure = $Pool_Port
-						User = ([Config]::WalletPlaceholder -f $Sign)
-						Password = Get-Join "," @("c=$Sign", $Pool_Diff, [Config]::WorkerNamePlaceholder)
-					})
-				}
-			}
-		}
-#>
 		[decimal] $Profit = ([Math]::Min($Algo.estimate_current, $Algo.actual_last24h) + $Algo.estimate_current * ((101 - $Algo.coins) / 100)) / 2
 		$Profit = $Profit * (1 - [decimal]$Algo.fees / 100) * $Pool_Variety / $Divisor
 		$ProfitFast = $Profit
