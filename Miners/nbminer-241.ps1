d433f723c88d95adf5f23e4f824d0e73cfeb2e26
Samsonov <samsonov@ckrt.ru>
2019-07-16 13:05:39 +0300

    update nbminer to 24.1

--- Miners/nbminer-241.ps1
+++ Miners/nbminer-241.ps1
@@ -53,7 +53,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "nbminer"
-					URI = "https://github.com/NebuTech/NBMiner/releases/download/v24.0/NBMiner_24.0_Win.zip"
+					URI = "https://github.com/NebuTech/NBMiner/releases/download/v24.1/NBMiner_24.1_Win.zip"
 					Path = "$Name\nbminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $stratum`://$($Pool.Host):$port -u $($Pool.User):$($Pool.Password) --api 127.0.0.1:4068 --no-nvml -no-watchdog $extrargs"
