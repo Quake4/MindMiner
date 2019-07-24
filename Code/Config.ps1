44374e5500b75ca9500d29b8966ad94a0bfdd83f
Samsonov <samsonov@ckrt.ru>
2019-07-24 11:48:36 +0300

    version 4.58

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -71,7 +71,7 @@ class Config : BaseConfig {
 	[bool] $ElectricityConsumption = $false
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v4.57"
+	static [string] $Version = "v4.58"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
