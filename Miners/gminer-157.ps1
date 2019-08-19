291600fd33a009dd88f6899aa3b18ad3f0191627
Oleg Samsonov <batsov@yandex.ru>
2019-08-20 00:10:51 +0300

    update gminer to 1.57

--- Miners/gminer-157.ps1
+++ Miners/gminer-157.ps1
@@ -91,7 +91,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.56/gminer_1_56_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.57/gminer_1_57_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $user -p $($Pool.Password) --api $port --pec 0 -w 0 $devs $extrargs"
