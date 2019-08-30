5f47f9600f953b6359c2dcad8195547b9d3433a6
Oleg Samsonov <batsov@yandex.ru>
2019-08-30 12:46:45 +0300

    update gminer to 1.61

--- Miners/gminer-161.ps1
+++ Miners/gminer-161.ps1
@@ -31,7 +31,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 
 if (!$Cfg.Enabled) { return }
 
-$AMD = @("aeternity", "beamhash", "beamhashII", "equihash144", "equihash192", "swap", "zhash")
+$AMD = @("aeternity", "beamhash", "beamhashII", "equihash125", "equihash144", "equihash192", "swap", "zhash")
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -91,7 +91,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.59/gminer_1_59_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.61/gminer_1_61_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $user -p $($Pool.Password) --api $port --pec 0 -w 0 $devs $extrargs"
