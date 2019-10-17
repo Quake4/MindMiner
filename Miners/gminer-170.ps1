9ff73d6116004eac9aed4e02084dea25d692dce3
Samsonov <samsonov@ckrt.ru>
2019-10-17 10:11:50 +0300

    update gminer to 1.70 and simplify

--- Miners/gminer-170.ps1
+++ Miners/gminer-170.ps1
@@ -17,12 +17,14 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "aeternity" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamhash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamhashII" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bfc" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckaroo29" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckarood29" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash125" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash144" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash192" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash96" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash125_4" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash144_5" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash192_7" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash96_5" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "grimm" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "grin31" } # all faster
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "swap" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "vds" }
@@ -31,7 +33,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 
 if (!$Cfg.Enabled) { return }
 
-$AMD = @("aeternity", "beamhash", "beamhashII", "eaglesong", "equihash125", "equihash144", "equihash192", "swap", "zhash")
+$AMD = @("aeternity", "beamhash", "beamhashII", "bfc", "eaglesong", "equihash125_4", "equihash144_5", "equihash192_7", "swap")
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -40,7 +42,7 @@ $Cfg.Algorithms | ForEach-Object {
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
 			if ($Pool) {
-				$alg = "-a $($_.Algorithm)"
+				if ($_.Algorithm -match "zhash") { $_.Algorithm = "equihash144_5" }
 				$types = if ([Config]::ActiveTypes -contains [eMinerType]::nVidia) { [eMinerType]::nVidia } else { $null }
 				if ($AMD -contains $_.Algorithm) {
 					if ([Config]::ActiveTypes -contains [eMinerType]::nVidia -and [Config]::ActiveTypes -contains [eMinerType]::AMD) {
@@ -50,27 +52,11 @@ $Cfg.Algorithms | ForEach-Object {
 						$types = [eMinerType]::AMD
 					}
 				}
-				if ($_.Algorithm -match "beamhash") {
-					$alg = "-a 150_5"
-				}
-				elseif ($_.Algorithm -match "equihash125") {
-					$alg = "-a 125_4"
-				}
-				elseif ($_.Algorithm -match "zhash" -or $_.Algorithm -match "equihash144") {
-					$alg = "-a 144_5"
-				}
-				elseif ($_.Algorithm -match "equihash192") {
-					$alg = "-a 192_7"
-				}
-				elseif ($_.Algorithm -match "equihash96") {
-					$alg = "-a 96_5"
-				}
-				if ($_.Algorithm -match "zhash" -or $_.Algorithm -match "equihash144" -or $_.Algorithm -match "equihash192" -or $_.Algorithm -match "equihash96") {
-					if (!($extrargs -match "-pers" -or $alg -match "-pers")) {
-						$alg = Get-Join " " @($alg, "--pers auto")
-					}
-				}
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				$alg = "-a $($_.Algorithm)"
+				if ($_.Algorithm -match "equihash" -and $extrargs -notmatch "-pers") {
+					$alg = Get-Join " " @($alg, "--pers auto")
+				}
 				$benchsecs = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 				$runbefore = $_.RunBefore
 				$runafter = $_.RunAfter
@@ -91,7 +77,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.68/gminer_1_68_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.70/gminer_1_70_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg -s $($Pool.Host) -n $($Pool.PortUnsecure) -u $user -p $($Pool.Password) --api $port --pec 0 -w 0 $devs $extrargs"
