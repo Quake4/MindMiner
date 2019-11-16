84cb6e714a67eddda0acfbc2f425ce4974d60f50
unknown <batsov@yandex.ru>
2019-11-16 13:01:41 +0300

    update amd xmrig to 5.0.0

---
+++ Miners/xmrig-amd-500.ps1
@@ -0,0 +1,56 @@
+<#
+MindMiner  Copyright (C) 2018-2019  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+if ([Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
+if (![Config]::Is64Bit) { exit }
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
+	Enabled = $true
+	BenchmarkSeconds = 120
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/0" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/r" }
+)}
+
+if (!$Cfg.Enabled) { return }
+
+$file = [IO.Path]::Combine($BinLocation, $Name, "config.json")
+if ([IO.File]::Exists($file)) {
+	[IO.File]::Delete($file)
+}
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::AMD
+					API = "xmrig2"
+					URI = "https://github.com/xmrig/xmrig/releases/download/v5.0.0/xmrig-5.0.0-gcc-win64.zip"
+					Path = "$Name\xmrig.exe"
+					ExtraArgs = $extrargs
+					Arguments = "-a $($_.Algorithm) -o $($Pool.Host):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) --api-port=4044 --donate-level=1 --opencl --opencl-platform=$([Config]::AMDPlatformId)  $extrargs"
+					Port = 4044
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+					Fee = 1
+				}
+			}
+		}
+	}
+}
