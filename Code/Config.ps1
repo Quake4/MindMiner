b3dfa1f354c3ea1fab1b2e69605e7d61fe149629
unknown <batsov@yandex.ru>
2019-11-26 21:50:50 +0300

    version 5.0

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -72,7 +72,7 @@ class Config : BaseConfig {
 	[decimal] $MaximumAllowedGrowth = 2
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v4.105"
+	static [string] $Version = "v5.0"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
