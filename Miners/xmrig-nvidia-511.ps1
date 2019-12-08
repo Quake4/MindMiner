35e26d9875740a928968c203019f969ca642e658
unknown <batsov@yandex.ru>
2019-12-08 13:03:12 +0300

    remap ncheavy

--- Miners/xmrig-nvidia-511.ps1
+++ Miners/xmrig-nvidia-511.ps1
@@ -18,6 +18,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/0" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/r" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/gpu" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/tube" }
 )}
 
 if (!$Cfg.Enabled) { return }
