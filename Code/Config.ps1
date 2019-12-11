6b3e3f29e0c43998ce33112a34dd1de7ea39d1e7
Samsonov <samsonov@ckrt.ru>
2019-12-11 10:32:05 +0300

    version 5.10

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -72,7 +72,7 @@ class Config : BaseConfig {
 	[decimal] $MaximumAllowedGrowth = 2
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v5.9"
+	static [string] $Version = "v5.10"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
