3a6629ce76bd9fe5f3e36290e74d42ebde717738
platinum4 <platinumstephen@gmail.com>
2019-12-11 15:15:58 -0600

    Update gminer to v1.8.3

--- Miners/gminer-183.ps1
+++ Miners/gminer-183.ps1
@@ -91,7 +91,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.82/gminer_1_82_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.83/gminer_1_83_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
