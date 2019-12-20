a7ca5ba7d77a685d2eadc221b04302f0af0066f1
unknown <batsov@yandex.ru>
2019-12-20 22:30:14 +0300

    update cpuminer-opt to 3.10.4

--- Miners/cpuminer-opt-3104.ps1
+++ Miners/cpuminer-opt-3104.ps1
@@ -128,7 +128,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "cpuminer"
-					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.10.3/cpuminer-opt-3.10.3-windows.zip"
+					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.10.4/cpuminer-opt-3.10.4-windows.zip"
 					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $extrargs"
