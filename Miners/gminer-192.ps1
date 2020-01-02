4f10d1ddcf26e276c645b8eb9175eafb610c0bbb
unknown <batsov@yandex.ru>
2020-01-03 00:01:24 +0300

    update gminer to 1.92 #569

--- Miners/gminer-192.ps1
+++ Miners/gminer-192.ps1
@@ -15,6 +15,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "aeternity" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bbc" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamhash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamhashII" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "blake2s" }
@@ -92,7 +93,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.88/gminer_1_88_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.92/gminer_1_92_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
