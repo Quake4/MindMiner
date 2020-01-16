3362d0e58db504978614e859d370cef035b64086
unknown <batsov@yandex.ru>
2020-01-16 20:24:28 +0300

    update lolminer to 0.9.5.1

--- Miners/lolminer-0951.ps1
+++ Miners/lolminer-0951.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2018-2019  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018-2020  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
@@ -16,20 +16,22 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beam" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamv2" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckatoo31" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckatoo32" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckarood29" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckaroom29" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash125" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash144" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash192" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash96" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihashBTG" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihashZCL" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "grin31" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "grind29" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "zhash" }
 )})
 
 if (!$Cfg.Enabled) { return }
 
-$url = "https://github.com/Lolliedieb/lolMiner-releases/files/3950113/lolMiner_v093_Win64.zip"
+$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.95/lolMiner_v0951_Win64.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -45,6 +47,10 @@ $Cfg.Algorithms | ForEach-Object {
 					switch ($_.Algorithm) {
 						"beam" { $coin = "--coin BEAM" }
 						"beamv2" { $coin = "--coin BEAM-II" }
+						"cuckatoo31" { $coin = "--coin GRIN-C31" }
+						"cuckatoo32" { $coin = "--coin GRIN-C32" }
+						"cuckarood29" { $coin = "--coin GRIN-C29D" }
+						"cuckaroom29" { $coin = "--coin GRIN-C29M" }
 						"zhash" { $coin = "--coin AUTO144_5" }
 						"equihash125" { $coin = "--coin ZEL" }
 						"equihash144" { $coin = "--coin AUTO144_5" }
@@ -52,8 +58,6 @@ $Cfg.Algorithms | ForEach-Object {
 						"equihash96" { $coin = "--coin MNX" }
 						"equihashBTG" { $coin = "--coin BTG" }
 						"equihashZCL" { $coin = "--coin AUTO192_7 --overwritePersonal ZcashPoW" }
-						"grin31" { $coin = "--coin GRIN-AT31" }
-						"grind29" { $coin = "--coin GRIN-AD29" }
 					}
 				}
 				$pools = "--pool $($Pool.Hosts -join ";") --port $(($Pool.Hosts | ForEach-Object { $Pool.PortUnsecure }) -join ";") --user $(($Pool.Hosts | ForEach-Object { $Pool.User }) -join ";") --pass $(($Pool.Hosts | ForEach-Object { $Pool.Password }) -join ";") --tls $(($Pool.Hosts | ForEach-Object { 0 }) -join ";")"
