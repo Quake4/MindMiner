bbcdd89ea37a6ec4a8d0a8e9b92b3e3747030d43
Samsonov <samsonov@ckrt.ru>
2020-01-17 16:22:15 +0300

    version 6.18

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -72,7 +72,7 @@ class Config : BaseConfig {
 	[decimal] $MaximumAllowedGrowth = 2
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v6.17"
+	static [string] $Version = "v6.18"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
