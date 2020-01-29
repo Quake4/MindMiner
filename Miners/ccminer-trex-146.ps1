d109350f404425eb352af7b04877ee9056d0b9e0
unknown <batsov@yandex.ru>
2020-01-29 23:43:32 +0300

    trex year

--- Miners/ccminer-trex-146.ps1
+++ Miners/ccminer-trex-146.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2018-2019  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018-2020  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
