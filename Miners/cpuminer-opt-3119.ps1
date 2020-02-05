66e137a79a9a994bfb76202b17cd995809e5c81b
platinum4 <platinumstephen@gmail.com>
2020-02-05 11:18:21 -0600

    cpuminer-opt-3119 with fix for API bind

--- Miners/cpuminer-opt-3119.ps1
+++ Miners/cpuminer-opt-3119.ps1
@@ -136,10 +136,10 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "cpuminer"
-					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.8/cpuminer-opt-3.11.8-windows.zip"
+					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.11.9/cpuminer-opt-3.11.9-windows.zip"
 					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 127.0.0.1:4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
 					Port = 4048
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
