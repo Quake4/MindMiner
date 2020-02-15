8751c80b271c62dc7c5b7c67178ac4d29bd0cda8
unknown <batsov@yandex.ru>
2020-02-15 18:58:19 +0300

    version 6.31

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -72,7 +72,7 @@ class Config : BaseConfig {
 	[decimal] $MaximumAllowedGrowth = 2
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v6.30"
+	static [string] $Version = "v6.31"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
