ce15363b2430854072cc58fd69a227348efef8f5
unknown <batsov@yandex.ru>
2020-02-26 21:38:52 +0300

    update gminer to 1.98 #641

--- Miners/gminer-198.ps1
+++ Miners/gminer-198.ps1
@@ -99,7 +99,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.97/gminer_1_97_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/1.98/gminer_1_98_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
