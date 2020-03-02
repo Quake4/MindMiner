3187c2422e4f479a25626e2484e4d97cc681067e
Samsonov <samsonov@ckrt.ru>
2020-03-02 08:45:56 +0300

    update srbm to 3.7.0 #643

--- Miners/srbm-cpu-37.ps1
+++ Miners/srbm-cpu-37.ps1
@@ -61,7 +61,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "srbm"
-					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.3.6/SRBMiner-Multi-0-3-6-win64.zip"
+					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.3.7/SRBMiner-Multi-0-3-7-win64.zip"
 					Path = "$Name\SRBMiner-MULTI.exe"
 					ExtraArgs = $extrargs
 					Arguments = "--algorithm $($_.Algorithm) --pool $($Pool.Hosts[0]):$($Pool.PortUnsecure) --wallet $($Pool.User) --password $($Pool.Password) --tls false --api-enable --api-port 4045 --cpu-priority 1 --disable-gpu --retry-time $($Config.CheckTimeout) $nicehash $extrargs"
