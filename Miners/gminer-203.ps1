150719d8c7e79e96a9e6c69fe301c380306007a1
unknown <batsov@yandex.ru>
2020-03-22 19:13:36 +0300

    update gminer to 2.03

--- Miners/gminer-203.ps1
+++ Miners/gminer-203.ps1
@@ -101,7 +101,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.02/gminer_2_02_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.03/gminer_2_03_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
