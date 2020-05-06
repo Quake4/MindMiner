9b094da11e27496db47181135f13facbdde9d524
unknown <batsov@yandex.ru>
2020-05-06 14:23:35 +0300

    better support nbminer

--- Miners/nbminer-302.ps1
+++ Miners/nbminer-302.ps1
@@ -47,7 +47,7 @@ $Cfg.Algorithms | ForEach-Object {
 				}
 				$stratum = $Pool.Protocol
 				$port = $Pool.Port
-				if ($Pool.Name -match "nicehash") { $stratum = "nicehash+tcp" }
+				if ($Pool.Name -match "nicehash" -and ($_.Algorithm -match "ethash" -or $_.Algorithm -match "cuck")) { $stratum = "nicehash+tcp" }
 				$pools = [string]::Empty
 				for ($i = 0; $i -lt $Pool.Hosts.Count -and $i -lt 3; $i++) {
 					$idx = if ($i -eq 0) { [string]::Empty } else { $i.ToString() }
