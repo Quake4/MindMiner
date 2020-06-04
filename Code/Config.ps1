cab134981abe15177cda44fc4d7d2acce15760d0
unknown <batsov@yandex.ru>
2020-06-04 14:32:22 +0300

    version 6.71

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -72,7 +72,7 @@ class Config : BaseConfig {
 	[decimal] $MaximumAllowedGrowth = 2
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v6.70"
+	static [string] $Version = "v6.71"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
