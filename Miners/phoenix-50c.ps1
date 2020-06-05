ada13c76a1682fbbac61328a99f4d7c6f3a17f48
unknown <batsov@yandex.ru>
2020-06-05 16:04:29 +0300

    update phoenix to 5.0c

--- Miners/phoenix-50c.ps1
+++ Miners/phoenix-50c.ps1
@@ -26,7 +26,7 @@ if ([IO.File]::Exists($file)) {
 	[IO.File]::Delete($file)
 }
 
-$url = "http://mindminer.online/miners/PhoenixMiner-50b.zip"
+$url = "http://mindminer.online/miners/PhoenixMiner-50c.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
