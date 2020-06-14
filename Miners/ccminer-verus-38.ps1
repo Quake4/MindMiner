02512d4b8a9fa3ccaa8019d9db554c9167e6bd4d
unknown <batsov@yandex.ru>
2020-06-14 19:40:43 +0300

    update verus miners to 0.3.8/0.3.7

--- Miners/ccminer-verus-38.ps1
+++ Miners/ccminer-verus-38.ps1
@@ -4,26 +4,21 @@ https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
 
-if ([Config]::ActiveTypes -notcontains [eMinerType]::CPU) { exit }
+if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
 if (![Config]::Is64Bit) { exit }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
 	Enabled = $true
-	BenchmarkSeconds = 150
+	BenchmarkSeconds = 120
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "verus" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "verus"; ExtraArgs = "-i 22" }
 )}
 
 if (!$Cfg.Enabled) { return }
-
-# choose version
-$bestminer = "ccminernonavx.exe"
-if ([Config]::CPUFeatures.Contains("AVX")) {
-	$bestminer = "ccminer.exe"
-}
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -40,16 +35,18 @@ $Cfg.Algorithms | ForEach-Object {
 					Priority = $Pool.Priority
 					Name = $Name
 					Algorithm = $Algo
-					Type = [eMinerType]::CPU
-					API = "ccminer_woe"
-					URI = "http://mindminer.online/miners/CPU/cpuminer-verus-361d.zip"
-					Path = "$Name\$bestminer"
+					Type = [eMinerType]::nVidia
+					API = "ccminer"
+					URI = "http://mindminer.online/miners/nVidia/ccminer-verus-38.zip"
+					Path = "$Name\ccminer.exe"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) -q --cpu-priority 1 -b 4048 -t $($Devices["CPU"].Cores) $N $extrargs"
-					Port = 4048
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) -q $N $extrargs"
+					Port = 4068
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
 					RunAfter = $_.RunAfter
+					# fix real speed lower than reported (6.35 => 5.75)
+					Fee = 10
 				}
 			}
 		}
--- Miners/cpuminer-verus-37.ps1
+++ Miners/cpuminer-verus-37.ps1
@@ -4,21 +4,26 @@ https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
 
-if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
+if ([Config]::ActiveTypes -notcontains [eMinerType]::CPU) { exit }
 if (![Config]::Is64Bit) { exit }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
 	Enabled = $true
-	BenchmarkSeconds = 120
+	BenchmarkSeconds = 150
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "verus" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "verus"; ExtraArgs = "-i 22" }
 )}
 
 if (!$Cfg.Enabled) { return }
+
+# choose version
+$bestminer = "ccminernonavx.exe"
+if ([Config]::CPUFeatures.Contains("AVX")) {
+	$bestminer = "ccminer.exe"
+}
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -35,18 +40,16 @@ $Cfg.Algorithms | ForEach-Object {
 					Priority = $Pool.Priority
 					Name = $Name
 					Algorithm = $Algo
-					Type = [eMinerType]::nVidia
-					API = "ccminer"
-					URI = "http://mindminer.online/miners/nVidia/ccminer-verus-3611d.zip"
-					Path = "$Name\ccminer.exe"
+					Type = [eMinerType]::CPU
+					API = "ccminer_woe"
+					URI = "http://mindminer.online/miners/CPU/cpuminer-verus-37.zip"
+					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) -q $N $extrargs"
-					Port = 4068
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) -q --cpu-priority 1 -b 4048 -t $($Devices["CPU"].Cores) $N $extrargs"
+					Port = 4048
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
 					RunAfter = $_.RunAfter
-					# fix real speed lower than reported (6.35 => 5.75)
-					Fee = 10
 				}
 			}
 		}
