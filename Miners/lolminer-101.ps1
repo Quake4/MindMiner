1aa917e644dd4720c93a9c66001979eac8858ac6
unknown <batsov@yandex.ru>
2020-06-28 15:26:55 +0300

    update lolminer to 1.0.1

--- Miners/lolminer-101.ps1
+++ Miners/lolminer-101.ps1
@@ -17,6 +17,7 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "aion" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beam" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamv2" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "beamv3" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckatoo31" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckatoo32" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckarood29" }
@@ -24,7 +25,7 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash125" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash144" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihash192" }
-		[AlgoInfoEx]@{ Enabled = $([Config]::ActiveTypes -notcontains [eMinerType]::nVidia); Algorithm = "equihash96" }
+		# [AlgoInfoEx]@{ Enabled = $([Config]::ActiveTypes -notcontains [eMinerType]::nVidia); Algorithm = "equihash96" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihashBTG" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "equihashZCL" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "zhash" }
@@ -32,7 +33,7 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 
 if (!$Cfg.Enabled) { return }
 
-$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/0.98/lolMiner_v098_Win64.zip"
+$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.01/lolMiner_v1.01_Win64.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -44,25 +45,29 @@ $Cfg.Algorithms | ForEach-Object {
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
 				$coin = [string]::Empty
 				$fee = 1
-				if ($extrargs -notmatch "--coin ") {
+				if ($extrargs -notmatch "--coin" -and $extrargs -notmatch "--algo") {
 					switch ($_.Algorithm) {
 						"aion" { $coin = "--coin AION" }
-						"beam" { $coin = "--coin BEAM" }
-						"beamv2" { $coin = "--coin BEAM-II" }
-						"cuckatoo31" { $coin = "--coin GRIN-C31"; $fee = 2 }
-						"cuckatoo32" { $coin = "--coin GRIN-C32"; $fee = 2 }
-						"cuckarood29" { $coin = "--coin MWC-C29D"; $fee = 2 }
+						"beam" { $coin = "--algo BEAM-I" }
+						"beamv2" { $coin = "--algo BEAM-II" }
+						"beamv3" { $coin = "--algo BEAM-III" }
+						"cuckatoo31" { $coin = "--algo C31"; $fee = 2 }
+						"cuckatoo32" { $coin = "--algo C32"; $fee = 2 }
+						"cuckarood29" { $coin = "--algo C29D"; $fee = 2 }
 						"cuckaroom29" { $coin = "--coin GRIN-C29M"; $fee = 2 }
 						"zhash" { $coin = "--coin AUTO144_5" }
-						"equihash125" { $coin = "--coin ZEL" }
+						"equihash125" { $coin = "--algo ZEL" }
 						"equihash144" { $coin = "--coin AUTO144_5" }
 						"equihash192" { $coin = "--coin AUTO192_7" }
-						"equihash96" { $coin = "--coin MNX" }
+						# "equihash96" { $coin = "--coin MNX" }
 						"equihashBTG" { $coin = "--coin BTG" }
-						"equihashZCL" { $coin = "--coin AUTO192_7 --overwritePersonal ZcashPoW" }
+						"equihashZCL" { $coin = "--coin ZCL" }
 					}
 				}
-				$pools = "--pool $($Pool.Hosts -join ";") --port $(($Pool.Hosts | ForEach-Object { $Pool.PortUnsecure }) -join ";") --user $(($Pool.Hosts | ForEach-Object { $Pool.User }) -join ";") --pass $(($Pool.Hosts | ForEach-Object { $Pool.Password }) -join ";") --tls $(($Pool.Hosts | ForEach-Object { 0 }) -join ";")"
+				$pools = [string]::Empty
+				$Pool.Hosts | ForEach-Object {
+					$pools = Get-Join " " @($pools, "--pool $_`:$($Pool.PortUnsecure) --user $($Pool.User) --pass $($Pool.Password) --tls 0")
+				}
 				[MinerInfo]@{
 					Pool = $Pool.PoolName()
 					PoolKey = $Pool.PoolKey()
@@ -75,7 +80,7 @@ $Cfg.Algorithms | ForEach-Object {
 					URI = $url
 					Path = "$Name\lolMiner.exe"
 					ExtraArgs = $extrargs
-					Arguments = "$coin $pools --apiport 4068 --timeprint 1 --disable_memcheck 1 --devices NVIDIA $extrargs"
+					Arguments = "$coin $pools --apiport 4068 --timeprint 1 --devices NVIDIA $extrargs"
 					Port = 4068
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
@@ -94,7 +99,7 @@ $Cfg.Algorithms | ForEach-Object {
 					URI = $url
 					Path = "$Name\lolMiner.exe"
 					ExtraArgs = $extrargs
-					Arguments = "$coin $pools --apiport 4028 --timeprint 1 --disable_memcheck 1 --devices AMD $extrargs"
+					Arguments = "$coin $pools --apiport 4028 --timeprint 1 --devices AMD $extrargs"
 					Port = 4028
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
