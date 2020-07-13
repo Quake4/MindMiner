28f99be8a8c4a42a8f215afa8d4acec8813f39ba
unknown <batsov@yandex.ru>
2020-07-13 20:55:20 +0300

    update gminer to 2.15

--- Miners/gminer-215.ps1
+++ Miners/gminer-215.ps1
@@ -23,6 +23,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "bfc" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cortex" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckaroo29" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckaroo29b" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckarood29" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckarood29v" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cuckaroom29" }
@@ -106,7 +107,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.14/gminer_2_14_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.15/gminer_2_15_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
