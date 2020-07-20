d0d5747cace963fac0ea65f89a7753e219333caa
Oleg Samsonov <batsov@yandex.ru>
2020-07-20 11:09:08 +0300

    Update and rename xmrig-amd-623.ps1 to xmrig-amd-630.ps1

--- Miners/xmrig-amd-630.ps1
+++ Miners/xmrig-amd-630.ps1
@@ -54,7 +54,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "xmrig2"
-					URI = "https://github.com/xmrig/xmrig/releases/download/v6.2.3/xmrig-6.2.3-gcc-win64.zip"
+					URI = "https://github.com/xmrig/xmrig/releases/download/v6.3.0/xmrig-6.3.0-gcc-win64.zip"
 					Path = "$Name\xmrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4044 --donate-level=1 --no-cpu --opencl --opencl-platform=$([Config]::AMDPlatformId) $extrargs"
