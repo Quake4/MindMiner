306fefeb16e23af79c1ee9bd222c86ba1cd86aa9
Oleg Samsonov <batsov@yandex.ru>
2020-07-20 11:06:39 +0300

    Update and rename gminer-218.ps1 to gminer-219.ps1

--- Miners/gminer-219.ps1
+++ Miners/gminer-219.ps1
@@ -108,7 +108,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.18/gminer_2_18_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.19/gminer_2_19_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
