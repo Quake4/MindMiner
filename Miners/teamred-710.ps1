6897765a9f48a0f5d36fc6fcde950d48097ea920
Samsonov <samsonov@ckrt.ru>
2020-09-14 09:18:35 +0300

    update teamred to 0.7.10

--- Miners/teamred-710.ps1
+++ Miners/teamred-710.ps1
@@ -71,7 +71,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "teamred"
-					URI = "https://github.com/todxx/teamredminer/releases/download/0.7.9/teamredminer-v0.7.9-win.zip"
+					URI = "https://github.com/todxx/teamredminer/releases/download/0.7.10/teamredminer-v0.7.10-win.zip"
 					Path = "$Name\teamredminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts --api_listen=127.0.0.1:4028 --platform=$([Config]::AMDPlatformId) $extrargs"
