e6e9daa9736194426ddf617e28fc8a9adbb33f65
madest13 <31035328+madest13@users.noreply.github.com>
2020-10-08 21:39:18 +0300

    Update and rename ccminer-trex-162.ps1 to ccminer-trex-172.ps1 (#711)
    Add ethash algorithm (for Ethereum). For Nicehash mining use nicehash+tcp:// pool url prefix.

--- Miners/ccminer-trex-172.ps1
+++ Miners/ccminer-trex-172.ps1
@@ -34,6 +34,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "mtp" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "phi" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "progpow" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "progpowz" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "polytimos" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sha256t" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "sha256q" }
@@ -56,10 +57,10 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 if (!$Cfg.Enabled) { return }
 
 switch ([Config]::CudaVersion) {
-	{ $_ -ge [version]::new(11, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.16.2/t-rex-0.16.2-win-cuda11.0.zip" }
-	{ $_ -ge [version]::new(10, 0) -and $_ -lt [version]::new(11, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.16.2/t-rex-0.16.2-win-cuda10.0.zip" }
-	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.16.2/t-rex-0.16.2-win-cuda9.2.zip" }
-	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.16.2/t-rex-0.16.2-win-cuda9.1.zip" }
+	{ $_ -ge [version]::new(11, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.17.2/t-rex-0.17.2-win-cuda11.0.zip" }
+	{ $_ -ge [version]::new(10, 0) -and $_ -lt [version]::new(11, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.17.2/t-rex-0.17.2-win-cuda10.0.zip" }
+	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.17.2/t-rex-0.17.2-win-cuda9.2.zip" }
+	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.17.2/t-rex-0.17.2-win-cuda9.1.zip" }
 }
 
 $Cfg.Algorithms | ForEach-Object {
