8dddb21d09dfceca22eaddf234f1da187f9b7d83
unknown <batsov@yandex.ru>
2020-10-26 10:18:24 +0300

    update gminer to 2.29

--- Miners/gminer-229.ps1
+++ Miners/gminer-229.ps1
@@ -109,7 +109,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.27/gminer_2_27_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.29/gminer_2_29_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api $port --pec 0 -w 0 $devs $extrargs"
