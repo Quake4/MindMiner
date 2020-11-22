f93c18968783af6dff840cb7e402d533bf110559
unknown <batsov@yandex.ru>
2020-11-22 22:25:29 +0300

    update phoenix to 5.2e #737

--- Miners/phoenix-52e.ps1
+++ Miners/phoenix-52e.ps1
@@ -26,7 +26,7 @@ if ([IO.File]::Exists($file)) {
 	[IO.File]::Delete($file)
 }
 
-$url = "http://mindminer.online/miners/PhoenixMiner-52c.zip"
+$url = "http://mindminer.online/miners/PhoenixMiner-52e.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
