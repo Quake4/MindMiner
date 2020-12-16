2f47165baab6a9632caa0596750b5b40195cbcd2
Oleg Samsonov <batsov@yandex.ru>
2020-12-17 00:48:25 +0300

    version 7.1

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -85,7 +85,7 @@ class Config : BaseConfig {
 	[Nullable[int]] $DefaultCPUThreads
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v7.0"
+	static [string] $Version = "v7.1"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
