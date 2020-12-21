6d09c2d0f7673b5883929a7502787b496c5d375a
Quake4 <batsov@yandex.ru>
2020-12-21 21:58:50 +0300

    update t-rex to 0.19.5

--- Miners/ccminer-trex-195.ps1
+++ Miners/ccminer-trex-195.ps1
@@ -6,7 +6,7 @@ License GPL-3.0
 
 if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
 if (![Config]::Is64Bit) { exit }
-if ([Config]::CudaVersion -lt [version]::new(9, 1)) { return }
+if ([Config]::CudaVersion -lt [version]::new(9, 2)) { return }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -64,10 +64,9 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 if (!$Cfg.Enabled) { return }
 
 switch ([Config]::CudaVersion) {
-	{ $_ -ge [version]::new(11, 1) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.4/t-rex-0.19.4-win-cuda11.1.zip" }
-	{ $_ -ge [version]::new(10, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.4/t-rex-0.19.4-win-cuda10.0.zip" }
-	([version]::new(9, 2)) { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.4/t-rex-0.19.4-win-cuda9.2.zip" }
-	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.4/t-rex-0.19.4-win-cuda9.1.zip" }
+	{ $_ -ge [version]::new(11, 1) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.5/t-rex-0.19.5-win-cuda11.1.zip" }
+	{ $_ -ge [version]::new(10, 0) } { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.5/t-rex-0.19.5-win-cuda10.0.zip" }
+	default { $url = "https://github.com/trexminer/T-Rex/releases/download/0.19.5/t-rex-0.19.5-win-cuda9.2.zip" }
 }
 
 $Cfg.Algorithms | ForEach-Object {
