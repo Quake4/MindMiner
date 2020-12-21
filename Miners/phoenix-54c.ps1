4fc3f88f3cba35533379a8b6c0c88a1b8f557fbb
Quake4 <batsov@yandex.ru>
2020-12-21 22:37:40 +0300

    update phoenix to 5.4c #793

--- Miners/phoenix-54c.ps1
+++ Miners/phoenix-54c.ps1
@@ -27,7 +27,7 @@ if ([IO.File]::Exists($file)) {
 	[IO.File]::Delete($file)
 }
 
-$url = "http://mindminer.online/miners/PhoenixMiner-54b.zip"
+$url = "http://mindminer.online/miners/PhoenixMiner-54c.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
