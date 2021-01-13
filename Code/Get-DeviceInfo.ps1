2a68e2114be53bf0303a3692df224e4e0695ad07
Quake4 <batsov@yandex.ru>
2021-01-13 10:58:53 +0300

    fix N/A in smi result #807

--- Code/Get-DeviceInfo.ps1
+++ Code/Get-DeviceInfo.ps1
@@ -262,7 +262,7 @@ function Get-Devices ([Parameter(Mandatory)] [eMinerType[]] $types, $olddevices)
 							}
 						}
 						else {
-							$vals = $_.Replace("GeForce ", [string]::Empty).Replace("[Not Supported]", "0").Replace("[Unknown Error]", "0").Split(",")
+							$vals = $_.Replace("GeForce ", [string]::Empty).Replace("[Not Supported]", "0").Replace("[N/A]", "0").Replace("[Unknown Error]", "0").Split(",")
 							$dpl = [MultipleUnit]::ToValueInvariant($vals[$header["power.default_limit"]], [string]::Empty)
 							if ($dpl -eq 0) { $dpl = 100 }
 							$gpuinfo = [GPUInfo]@{
