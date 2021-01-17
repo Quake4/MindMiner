73cffec326c8e04221792d0a1695d4f22183b5a3
Quake4 <batsov@yandex.ru>
2021-01-17 11:51:14 +0300

    update bminer to 16.4.5 #812

--- Miners/bminer-1645.ps1
+++ Miners/bminer-1645.ps1
@@ -89,7 +89,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "bminer"
-					URI = "https://www.bminercontent.com/releases/bminer-lite-v16.4.4-f58a91d-amd64.zip"
+					URI = "https://www.bminercontent.com/releases/bminer-lite-v16.4.5-53c7f21-amd64.zip"
 					Path = "$Name\bminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-uri $hosts -watchdog=false -api 127.0.0.1:1880 $pers $extrargs"
