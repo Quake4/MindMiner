b88eefd72c3b67a395c6bd710e9cddfcb0f8b7a0
Quake4 <batsov@yandex.ru>
2021-03-23 13:01:33 +0300

    update cpuminer-opt to 3.16.0 #830

--- Miners/cpuminer-opt-3160.ps1
+++ Miners/cpuminer-opt-3160.ps1
@@ -71,6 +71,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "tribus" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "veltor" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "veil" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "verthash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x11evo" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "x11gost" }
 		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "x12" }
@@ -165,7 +166,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "cpuminer"
-					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.15.7/cpuminer-opt-3.15.7-windows.zip"
+					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.16.0/cpuminer-opt-3.16.0-windows.zip"
 					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 127.0.0.1:4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
