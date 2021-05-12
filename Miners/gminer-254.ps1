3baa2269a4511737ddf84b59b9a77d4559b539f1
Quake4 <batsov@yandex.ru>
2021-05-12 17:27:27 +0300

    update gminer to 2.54

--- Miners/gminer-254.ps1
+++ Miners/gminer-254.ps1
@@ -106,7 +106,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.53/gminer_2_53_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api 127.0.0.1:$port $pec-w 0 $devs $extrargs"
