44ce6bee304c82a6206c440b1e326d2e9797979d
Quake4 <batsov@yandex.ru>
2021-05-17 21:22:09 +0300

    fix year

--- Code/Out-DeviceInfo.ps1
+++ Code/Out-DeviceInfo.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2017-2018  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2017-2021  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
@@ -148,7 +148,6 @@ function Get-DevicesForApi ([Parameter(Mandatory)] [eMinerType] $type) {
 					@{ Label="threads"; Expression = { $_.Threads } }
 					@{ Label="clock"; Expression = { $_.Clock } }
 					@{ Label="load"; Expression = { $_.Load } }
-					# ? Features
 				))
 				if ($global:Admin) {
 					$columnsapi.AddRange(@(
