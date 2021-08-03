4945226e2fc204dcd85d35d11d51a95bbd7cd887
Quake4 <batsov@yandex.ru>
2021-08-03 09:16:46 +0300

    better mrr support

--- Pools/MRR.ps1
+++ Pools/MRR.ps1
@@ -365,11 +365,12 @@ try {
 				$Price = [decimal]$_.price.BTC.price / [MultipleUnit]::ToValueInvariant("1", $_.price.type.ToLower().TrimEnd("h"))
 				$SpeedCalc = (($KnownAlgos.Values | Foreach-Object { $t = $_[$alg]; if ($t) { $t } }) | Measure-Object Speed -Sum).Sum
 				$warn = if ($SpeedCalc * 0.95 -gt $SpeedAdv -or $SpeedCalc * 1.05 -lt $SpeedAdv) { " != $([MultipleUnit]::ToString($SpeedCalc) -replace `" `")" } else { [string]::Empty }
+				$color = if ($SpeedCalc * 1.05 -lt $SpeedAdv) { "Red" } else { "Yellow" }
 				$thash = $Algos[$alg].Extra["totalhash"]
 				$hashpercent = if ($thash -gt 0 -and ($thash - $SpeedAdv) -gt 0) { "($([decimal]::Round($SpeedAdv * 100 / ($thash - $SpeedAdv), 2))%) " } else { [string]::Empty }
 				Write-Host "MRR: Online $alg ($(Get-Join ", " $KnownTypes)), $([decimal]::Round($SpeedAdv * $Price, 8)), $hashpercent" -NoNewline
 				if (![string]::IsNullOrWhiteSpace($warn)) {
-					Write-Host "$($_.hashrate.advertised.nice)$warn`H/s" -NoNewline -ForegroundColor Red
+					Write-Host "$($_.hashrate.advertised.nice)$warn`H/s" -NoNewline -ForegroundColor $color
 				} else {
 					Write-Host "$($_.hashrate.advertised.nice)$warn`H/s" -NoNewline
 				}
