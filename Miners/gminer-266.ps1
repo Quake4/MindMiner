9298467491056fa24871e7ab33c354fd2f55e5d9
Quake4 <batsov@yandex.ru>
2021-09-07 16:31:43 +0300

    update gminer to 2.66 #851

--- Miners/gminer-266.ps1
+++ Miners/gminer-266.ps1
@@ -99,7 +99,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.63/gminer_2_63_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.66/gminer_2_66_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api 127.0.0.1:$port $pec-w 0 $devs $contest $extrargs"
