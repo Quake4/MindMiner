0f80982290c6660d6f1af807eefc7909855de938
Quake4 <batsov@yandex.ru>
2021-09-24 23:54:23 +0300

    update wildrig to 0.29.0

--- Miners/wildrig-nvidia-290.ps1
+++ Miners/wildrig-nvidia-290.ps1
@@ -102,7 +102,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Type = [eMinerType]::nVidia
 					TypeInKey = $true
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.28.3/wildrig-multi-windows-0.28.3.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.29.0/wildrig-multi-windows-0.29.0.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts -R $($Config.CheckTimeout) --opencl-platform=$([Config]::nVidiaPlatformId) --no-adl --api-port=4068 --donate-level=1 $extrargs"
