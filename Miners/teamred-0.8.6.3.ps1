2d10900a80b03ff507f9e3db5a56ffb7f3c8d4c0
Quake4 <batsov@yandex.ru>
2021-11-11 19:36:17 +0300

    update teamred to 0.8.6.3

--- Miners/teamred-0.8.6.3.ps1
+++ Miners/teamred-0.8.6.3.ps1
@@ -75,7 +75,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "teamred"
-					URI = "https://github.com/todxx/teamredminer/releases/download/v0.8.6.2/teamredminer-v0.8.6.2-win.zip"
+					URI = "https://github.com/todxx/teamredminer/releases/download/v0.8.6.3/teamredminer-v0.8.6.3-win.zip"
 					Path = "$Name\teamredminer.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts --api_listen=127.0.0.1:4028 --platform=$([Config]::AMDPlatformId) $extrargs"
