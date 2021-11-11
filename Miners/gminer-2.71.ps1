19b9f0e41f199c8cbfa654178e6c4cadec729bf3
Quake4 <batsov@yandex.ru>
2021-11-11 18:25:17 +0300

    update gminer to 2.71 #858

--- Miners/gminer-2.71.ps1
+++ Miners/gminer-2.71.ps1
@@ -95,7 +95,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.70/gminer_2_70_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.71/gminer_2_71_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api 127.0.0.1:$port $pec-w 0 $devs $extrargs"
