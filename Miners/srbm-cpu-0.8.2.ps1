8a2a62bd9d54981c5a1861c70091d02b42481fa7
Quake4 <batsov@yandex.ru>
2021-11-16 13:35:06 +0300

    add cnheavy to srbm-cpu #877

--- Miners/srbm-cpu-0.8.2.ps1
+++ Miners/srbm-cpu-0.8.2.ps1
@@ -30,6 +30,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "circcash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cpupower"; ExtraArgs = $extraCores }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonight_turtle" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonight_heavyx"; ExtraArgs = $extraThreads }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonight_upx"; ExtraArgs = $extraThreads }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cryptonight_xhv" } # L3 limit
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "curvehash"; ExtraArgs = $extraThreads }
