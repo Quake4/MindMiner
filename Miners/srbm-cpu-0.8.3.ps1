91108968de9215f92ac130c0638bad7fd86181ba
Quake4 <batsov@yandex.ru>
2021-11-18 20:57:33 +0300

    update srbm to 0.8.3

--- Miners/srbm-cpu-0.8.3.ps1
+++ Miners/srbm-cpu-0.8.3.ps1
@@ -112,7 +112,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "srbm2"
-					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.2/SRBMiner-Multi-0-8-2-win64.zip"
+					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.3/SRBMiner-Multi-0-8-3-win64.zip"
 					Path = "$Name\SRBMiner-MULTI.exe"
 					ExtraArgs = $extrargs
 					Arguments = "--algorithm $($_.Algorithm) --pool $pools --wallet $($Pool.User) --password $($Pool.Password) --tls false --api-enable --api-port 4045 --miner-priority 1 --disable-gpu --retry-time $($Config.CheckTimeout) $nicehash $extrargs"
