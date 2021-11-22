fafc7e2778e13a476b50f6e8084ce86f3e75899b
Quake4 <batsov@yandex.ru>
2021-11-22 12:17:01 +0300

    update wildrig to 0.30.5

--- Miners/wildrig-amd-0.30.5.ps1
+++ Miners/wildrig-amd-0.30.5.ps1
@@ -104,7 +104,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Type = [eMinerType]::AMD
 					TypeInKey = $true
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.30.3/wildrig-multi-windows-0.30.3.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.30.5/wildrig-multi-windows-0.30.5.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts -R $($Config.CheckTimeout) --opencl-platform=$([Config]::AMDPlatformId) --no-nvml --api-port=4028 --donate-level=1 $extrargs"
