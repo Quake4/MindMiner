8069e659c4491f7cde0c4df7408a47179220a4b1
Quake4 <batsov@yandex.ru>
2021-11-23 10:05:02 +0300

    fix tide mapping

--- MindMiner.ps1
+++ MindMiner.ps1
@@ -377,7 +377,7 @@ while ($true)
 			"x13sm3" = "Hsr"
 			"x16rtgin" = "X16rt"
 			"yespower2b" = "Power2b"
-			"yespowertdc" = "yespowertide"
+			"yespowertdc" = "Yespowertide"
 		})
 		# disable asic algorithms
 		$AllAlgos.Add("Disabled", @("bcd", "beam", "bitcore", "blake", "blake2b", "blake2s", "handshake", "kadena", "sha256", "sha256t", "sha256asicboost", "sha256-ld", "sha3d", "scrypt", "scrypt-ld", "tensority", "x11", "x11-ld", "x13", "x14", "x15", "quark", "qubit", "myrgr", "lbry", "decred", "sia", "blake", "nist5", "cryptonight", "cryptonightr", "cryptonightv7", "cryptonightv8", "cnheavy", "cnsaber", "x11gost", "groestl", "eaglesong", "equihash", "lyra2re2", "lyra2z", "pascal", "keccak", "keccakc", "skein", "tribus", "c11", "phi", "timetravel", "skunk"))
