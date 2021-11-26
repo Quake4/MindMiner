2b8671c3b0ace44ad5e2afc70ebf9cbb7bc04fea
Quake4 <batsov@yandex.ru>
2021-11-27 02:33:18 +0300

    update xmrig to 6.16.0

--- Miners/xmrig-nvidia-6.16.0.ps1
+++ Miners/xmrig-nvidia-6.16.0.ps1
@@ -4,8 +4,9 @@ https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
 
-if ([Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
+if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
 if (![Config]::Is64Bit) { exit }
+if ([Config]::CudaVersion -lt [version]::new(10, 1)) { return }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
@@ -15,6 +16,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 	ExtraArgs = $null
 	Algorithms = @(
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2/wrkz" }
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "kawpow" } # t-tex faster
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/ccx" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/r" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/rwz" }
@@ -22,7 +24,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/tube" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/xhv" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/upx2" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "kawpow" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "gr" }
 		[AlgoInfoEx]@{ Enabled = $([Config]::ActiveTypes -notcontains [eMinerType]::CPU); Algorithm = "rx/0" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/arq" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/graft" }
@@ -56,13 +58,13 @@ $Cfg.Algorithms | ForEach-Object {
 					Priority = $Pool.Priority
 					Name = $Name
 					Algorithm = $Algo
-					Type = [eMinerType]::AMD
+					Type = [eMinerType]::nVidia
 					API = "xmrig2"
-					URI = "https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-gcc-win64.zip"
+					URI = "https://mindminer.online/miners/nVidia/xmrig-6.16.0-msvc-win64.zip"
 					Path = "$Name\xmrig.exe"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4044 --donate-level=1 --no-dmi --no-cpu --opencl --opencl-platform=$([Config]::AMDPlatformId) $extrargs"
-					Port = 4044
+					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4043 --donate-level=1 --no-dmi --no-cpu --cuda --no-nvml $extrargs"
+					Port = 4043
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
 					RunAfter = $_.RunAfter
