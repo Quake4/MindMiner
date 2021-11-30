56300ef79c4747f134e5a7f8a53175db1b552fa1
Quake4 <batsov@yandex.ru>
2021-11-30 17:07:51 +0300

    update wildrig to 0.31.1

--- Miners/wildrig-nvidia-0.31.1.ps1
+++ Miners/wildrig-nvidia-0.31.1.ps1
@@ -106,7 +106,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Type = [eMinerType]::nVidia
 					TypeInKey = $true
 					API = "xmrig"
-					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.31.0/wildrig-multi-windows-0.31.0.7z"
+					URI = "https://github.com/andru-kun/wildrig-multi/releases/download/0.31.1/wildrig-multi-windows-0.31.1.7z"
 					Path = "$Name\wildrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts -R $($Config.CheckTimeout) --opencl-platform=$([Config]::nVidiaPlatformId) --no-adl --api-port=4068 --donate-level=1 $extrargs"
