0dd2385dd2b4592c0667e699497c76d9efe66997
Quake4 <batsov@yandex.ru>
2021-12-01 21:01:17 +0300

    update srbm to 0.8.5

--- Miners/srbm-cpu-0.8.5.ps1
+++ Miners/srbm-cpu-0.8.5.ps1
@@ -88,7 +88,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 if (!$Cfg.Enabled) { return }
 
 $Cfg.Algorithms | ForEach-Object {
-	if ($_.Enabled -and $_.Algorithm -notmatch "verushash") {
+	if ($_.Enabled) {
 		$Algo = Get-Algo($_.Algorithm)
 		if ($Algo) {
 			# find pool by algorithm
@@ -116,7 +116,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "srbm2"
-					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.4/SRBMiner-Multi-0-8-4-win64.zip"
+					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-win64.zip"
 					Path = "$Name\SRBMiner-MULTI.exe"
 					ExtraArgs = $extrargs
 					Arguments = "--algorithm $($_.Algorithm) --pool $pools --wallet $($Pool.User) --password $($Pool.Password) --tls false --api-enable --api-port 4045 --miner-priority 1 --disable-gpu --retry-time $($Config.CheckTimeout) $nicehash $extrargs"
