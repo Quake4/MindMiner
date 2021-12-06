e12274fcb3248eee5b59125fc8a2a975caebd2e1
Quake4 <batsov@yandex.ru>
2021-12-06 11:17:20 +0300

    nb working on mrr

--- Miners/nb-amd-40.1.ps1
+++ Miners/nb-amd-40.1.ps1
@@ -29,7 +29,7 @@ $Cfg.Algorithms | ForEach-Object {
 		if ($Algo -and $_.Algorithm -notmatch "bfc") { # https://github.com/NebuTech/NBMiner/issues/154
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
-			if ($Pool -and ($Pool.Name -notmatch "mrr" -or ($Pool.Name -match "mrr" -and $_.Algorithm -notmatch "cuckoo_ae" -and $_.Algorithm -notmatch "cuckarood")) -and
+			if ($Pool -and ($Pool.Name -notmatch "mrr" -or ($Pool.Name -match "mrr" -and $_.Algorithm -notmatch "cuckarood")) -and
 				($Pool.Name -notmatch "mph" -or ($Pool.Name -match "mph" -and $_.Algorithm -notmatch "ethash"))) {
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
 				$fee = 2
