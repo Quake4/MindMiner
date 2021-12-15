5f20d21448eafcdffe87574ccd2eefb85b224016
Quake4 <batsov@yandex.ru>
2021-12-15 21:05:09 +0300

    update srbm to 0.8.7

--- Miners/srbm-amd-0.8.7.ps1
+++ Miners/srbm-amd-0.8.7.ps1
@@ -85,7 +85,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::AMD
 					API = "srbm2"
-					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-win64.zip"
+					URI = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.7/SRBMiner-Multi-0-8-7-win64.zip"
 					Path = "$Name\SRBMiner-MULTI.exe"
 					ExtraArgs = $extrargs
 					Arguments = "--algorithm $($_.Algorithm) --pool $pools --wallet $($Pool.User) --password $($Pool.Password) --tls false --api-enable --api-port 4044 --disable-cpu --retry-time $($Config.CheckTimeout) $nicehash $extrargs"
