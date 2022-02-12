6dd73f259c0eff817f5e8ef7745d23565a729aea
Quake4 <batsov@yandex.ru>
2022-02-12 17:07:03 +0300

    update xmrig to 6.16.4

--- Miners/xmrig-cpu-6.16.4.ps1
+++ Miners/xmrig-cpu-6.16.4.ps1
@@ -1,36 +1,43 @@
 <#
-MindMiner  Copyright (C) 2018-2021  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018-2022  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
 
-if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
+if ([Config]::ActiveTypes -notcontains [eMinerType]::CPU) { exit }
 if (![Config]::Is64Bit) { exit }
-if ([Config]::CudaVersion -lt [version]::new(10, 1)) { return }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$extraThreads = $null
+$extraCores = $null
+if ([Config]::DefaultCPU) {
+	$extraThreads = "-t $([Config]::DefaultCPU.Threads)"
+	$extraCores = "-t $([Config]::DefaultCPU.Cores)"
+}
 
 $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename)) @{
 	Enabled = $true
 	BenchmarkSeconds = 120
 	ExtraArgs = $null
 	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "argon2/chukwa" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2/chukwav2" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "argon2/wrkz" }
-		[AlgoInfoEx]@{ Enabled = $false; Algorithm = "kawpow" } # t-tex faster
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/ccx" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/ccx"; ExtraArgs = $extraCores }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/r" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/rwz" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/zls" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/tube" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/xhv" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/upx2" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "gr" }
-		[AlgoInfoEx]@{ Enabled = $([Config]::ActiveTypes -notcontains [eMinerType]::CPU); Algorithm = "rx/0" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/arq" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn-heavy/xhv" } # L3 limit
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "cn/upx2"; ExtraArgs = $extraThreads }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "gr"; ExtraArgs = $extraCores }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/0"; ExtraArgs = $extraThreads }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/arq"; ExtraArgs = $extraThreads }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/graft" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/keva" }
-		[AlgoInfoEx]@{ Enabled = $([Config]::ActiveTypes -notcontains [eMinerType]::CPU); Algorithm = "rx/sfx" }
-		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/wow" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/sfx"; ExtraArgs = $extraCores }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rx/wow"; ExtraArgs = $extraThreads }
 )}
 
 if (!$Cfg.Enabled) { return }
@@ -43,7 +50,7 @@ if ([IO.File]::Exists($file)) {
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
 		$Algo = Get-Algo($_.Algorithm)
-		if ($Algo -and $Algo -notmatch "chukwa") {
+		if ($Algo) {
 			# find pool by algorithm
 			$Pool = Get-Pool($Algo)
 			if ($Pool) {
@@ -58,13 +65,13 @@ $Cfg.Algorithms | ForEach-Object {
 					Priority = $Pool.Priority
 					Name = $Name
 					Algorithm = $Algo
-					Type = [eMinerType]::nVidia
+					Type = [eMinerType]::CPU
 					API = "xmrig2"
-					URI = "https://mindminer.online/miners/nVidia/xmrig-6.16.3-msvc-win64.zip"
+					URI = "https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-gcc-win64.zip"
 					Path = "$Name\xmrig.exe"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4043 --donate-level=1 --no-dmi --no-cpu --cuda --no-nvml $extrargs"
-					Port = 4043
+					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4045 --donate-level=1 --no-dmi --cpu-priority 0 $extrargs"
+					Port = 4045
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
 					RunAfter = $_.RunAfter
