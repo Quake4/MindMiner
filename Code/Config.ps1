77e12c49fc89aec3e3d4da4e1698aeac2bead4c1
Quake4 <batsov@yandex.ru>
2022-02-13 21:54:54 +0300

    version 7.67

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -85,7 +85,7 @@ class Config : BaseConfig {
 	[Nullable[int]] $DefaultCPUThreads
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v7.66"
+	static [string] $Version = "v7.67"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
