053f78e829a1c26ae49b40cf18fa7cd7a8375150
Quake4 <batsov@yandex.ru>
2022-02-27 17:26:55 +0300

    update gminer to 2.82

--- Miners/gminer-2.82.ps1
+++ Miners/gminer-2.82.ps1
@@ -92,7 +92,7 @@ $Cfg.Algorithms | ForEach-Object {
 							Type = $_
 							TypeInKey = $true
 							API = "gminer"
-							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.79/gminer_2_79_windows64.zip"
+							URI = "https://github.com/develsoftware/GMinerRelease/releases/download/2.82/gminer_2_82_windows64.zip"
 							Path = "$Name\miner.exe"
 							ExtraArgs = $extrargs
 							Arguments = "$alg $hosts --api 127.0.0.1:$port $pec-w 0 $devs $extrargs"
