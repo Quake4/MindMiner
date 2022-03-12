03b852b83f2d25c2ccd3897b070900aaaf70f936
Oleg Samsonov <batsov@yandex.ru>
2022-03-12 22:10:42 +0300

    version 7.83

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -86,7 +86,7 @@ class Config : BaseConfig {
 	$Service = $null
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v7.82"
+	static [string] $Version = "v7.83"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
