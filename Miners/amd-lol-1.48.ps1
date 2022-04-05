52691e5745015b418a3770dfc60ec4e2fa90a8a3
Quake4 <batsov@yandex.ru>
2022-04-05 22:14:51 +0300

    update lol to 1.48

--- Miners/amd-lol-1.48.ps1
+++ Miners/amd-lol-1.48.ps1
@@ -40,7 +40,7 @@ $Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [Bas
 
 if (!$Cfg.Enabled) { return }
 
-$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Win64.zip"
+$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Win64.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
