1398d8809aae64f62953fb1c4750816b34cbfa2f
Quake4 <batsov@yandex.ru>
2022-05-11 10:45:30 +0300

    update lol miner to 1.49

--- Miners/amd-lol-1.49.ps1
+++ Miners/amd-lol-1.49.ps1
@@ -40,7 +40,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 
 if (!$Cfg.Enabled) { return }
 
-$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Win64.zip"
+$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.49/lolMiner_v1.49_Win64.zip"
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
