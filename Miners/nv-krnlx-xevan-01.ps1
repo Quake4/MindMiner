0824d60e9b36283173ed3dfe6410e42523541b1f
Quake4 <batsov@yandex.ru>
2022-05-29 16:39:39 +0300

    reborn krnlx xevan miner

---
+++ Miners/nv-krnlx-xevan-01.ps1
@@ -0,0 +1,59 @@
+<#
+MindMiner  Copyright (C) 2017-2022  Oleg Samsonov aka Quake4
+https://github.com/Quake4/MindMiner
+License GPL-3.0
+#>
+
+. .\Code\Include.ps1
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Cfg = [BaseConfig]::ReadOrCreate([IO.Path]::Combine($PSScriptRoot, $Name + [BaseConfig]::Filename), @{
+	Enabled = $true
+	BenchmarkSeconds = 90
+	ExtraArgs = $null
+	Algorithms = @(
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "xevan" }
+)})
+
+if (!$Cfg.Enabled) { return }
+
+if ([Config]::Is64Bit -eq $true) {
+	$url = "https://github.com/krnlx/ccminer-xevan/releases/download/0.1/ccminer.exe"
+	$file = "ccminer.exe"
+}
+else {
+	$url = "https://github.com/krnlx/ccminer-xevan/releases/download/0.1/ccminer_x86.exe"
+	$file = "ccminer_x86.exe"
+}
+
+$Cfg.Algorithms | ForEach-Object {
+	if ($_.Enabled) {
+		$Algo = Get-Algo($_.Algorithm)
+		if ($Algo) {
+			# find pool by algorithm
+			$Pool = Get-Pool($Algo)
+			if ($Pool) {
+				$N = Get-CCMinerStatsAvg $Algo $_
+				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
+				[MinerInfo]@{
+					Pool = $Pool.PoolName()
+					PoolKey = $Pool.PoolKey()
+					Priority = $Pool.Priority
+					Name = $Name
+					Algorithm = $Algo
+					Type = [eMinerType]::nVidia
+					API = "ccminer"
+					URI = $url
+					Path = "$Name\$file"
+					ExtraArgs = $extrargs
+					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Host[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -R $($Config.CheckTimeout) $N $extrargs"
+					Port = 4068
+					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
+					RunBefore = $_.RunBefore
+					RunAfter = $_.RunAfter
+				}
+			}
+		}
+	}
+}
