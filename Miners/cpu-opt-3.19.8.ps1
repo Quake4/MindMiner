b1c164285c82e41ca9589d313cc0a6190b52ed89
Quake4 <batsov@yandex.ru>
2022-05-29 16:55:11 +0300

    update cpu-opt to 3.19.8 and add ssl support

--- Miners/cpu-opt-3.19.8.ps1
+++ Miners/cpu-opt-3.19.8.ps1
@@ -166,10 +166,10 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "cpuminer"
-					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.19.7/cpuminer-opt-3.19.7-windows.zip"
+					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.19.8/cpuminer-opt-3.19.8-windows.zip"
 					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) -o stratum+tcp://$($Pool.Hosts[0]):$($Pool.PortUnsecure) -u $($Pool.User) -p $($Pool.Password) -q -b 127.0.0.1:4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
+					Arguments = "-a $($_.Algorithm) -o $($Pool.Protocol)://$($Pool.Hosts[0]):$($Pool.Port) -u $($Pool.User) -p $($Pool.Password) -q -b 127.0.0.1:4048 --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
 					Port = 4048
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
