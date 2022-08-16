175e0fad1d9138525d68e5042fab1977258189fa
Quake4 <batsov@yandex.ru>
2022-08-16 22:28:44 +0300

    update t-rex miner to 0.26.5

--- Miners/nv-trex-0.26.5.ps1
+++ Miners/nv-trex-0.26.5.ps1
@@ -69,7 +69,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "trex"
-					URI = "https://trex-miner.com/download/t-rex-0.26.4-win.zip"
+					URI = "https://trex-miner.com/download/t-rex-0.26.5-win.zip"
 					Path = "$Name\t-rex.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts -R $($Config.CheckTimeout) --api-bind-http 127.0.0.1:4068 --api-read-only --no-strict-ssl --no-watchdog --gpu-report-interval 60 $N $extrargs"
