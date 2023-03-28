58d0adb0f902f29435b01b9c828d4be72b24b87f
Quake4 <batsov@yandex.ru>
2023-03-28 09:42:53 +0300

    update cpuminer opt to 0.22.1

--- Miners/cpu-opt-3.22.1.ps1
+++ Miners/cpu-opt-3.22.1.ps1
@@ -169,7 +169,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "cpuminer"
-					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.21.1/cpuminer-opt-3.21.1-windows.zip"
+					URI = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.22.1/cpuminer-opt-3.22.1-windows.zip"
 					Path = "$Name\$bestminer"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) -o $($Pool.Protocol)://$($Pool.Hosts[0]):$($Pool.Port) -u $($Pool.User) -p $($Pool.Password) -q -b 127.0.0.1:$port --cpu-priority 1 --retry-pause $($Config.CheckTimeout) -T 500 $add $extrargs"
