f82c18059184620261014f04faf6dad34ba6cf14
Quake4 <batsov@yandex.ru>
2023-06-03 22:28:42 +0300

    update bzminer to 15.2.0

--- Miners/bz-amd-15.2.0.ps1
+++ Miners/bz-amd-15.2.0.ps1
@@ -4,7 +4,7 @@ https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
 
-if ([Config]::ActiveTypes -notcontains [eMinerType]::nVidia) { exit }
+if ([Config]::ActiveTypes -notcontains [eMinerType]::AMD) { exit }
 if (![Config]::Is64Bit) { exit }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -18,10 +18,17 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "ergo" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "etchash" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "ethash" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "ironfish" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "ixi" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "kaspa" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "kylacoin" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "meowcoin" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "neox" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "nexa" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "novo" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "octa" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "olhash" }
+		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "radiant" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "rvn" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "woodcoin" }
 		[AlgoInfoEx]@{ Enabled = $true; Algorithm = "zil" }
@@ -29,7 +36,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 
 if (!$Cfg.Enabled) { return }
 
-$port = [Config]::Ports[[int][eMinerType]::nVidia]
+$port = [Config]::Ports[[int][eMinerType]::AMD]
 
 $Cfg.Algorithms | ForEach-Object {
 	if ($_.Enabled) {
@@ -40,7 +47,7 @@ $Cfg.Algorithms | ForEach-Object {
 			if ($Pool) {
 				$extrargs = Get-Join " " @($Cfg.ExtraArgs, $_.ExtraArgs)
 				$fee = 0.5
-				if (("olhash", "kaspa", "ixi") -contains $_.Algorithm) { $fee = 1 }
+				if (("olhash", "kaspa", "ixi", "woodcoin") -contains $_.Algorithm) { $fee = 1 }
 				elseif ($_.Algorithm -match "zil") { $fee = 0 }
 				[MinerInfo]@{
 					Pool = $Pool.PoolName()
@@ -48,12 +55,12 @@ $Cfg.Algorithms | ForEach-Object {
 					Priority = $Pool.Priority
 					Name = $Name
 					Algorithm = $Algo
-					Type = [eMinerType]::nVidia
+					Type = [eMinerType]::AMD
 					API = "bzminer"
-					URI = "https://www.bzminer.com/downloads/bzminer_v11.1.0_windows.zip"
+					URI = "https://www.bzminer.com/downloads/bzminer_v15.2.0_windows.zip"
 					Path = "$Name\bzminer.exe"
 					ExtraArgs = $extrargs
-					Arguments = "-a $($_.Algorithm) -p $($Pool.Hosts[0]):$($Pool.PortUnsecure) -w $($Pool.User) --pool_password $($Pool.Password) --no_watchdog --nvidia 1 --amd 0 --nc 1 --update_frequency_ms 60000 --http_address 127.0.0.1 --http_port $port $extrargs"
+					Arguments = "-a $($_.Algorithm) -p $($Pool.Hosts[0]):$($Pool.PortUnsecure) -w $($Pool.User) --pool_password $($Pool.Password) --no_watchdog --nvidia 0 --amd 1 --nc 1 --update_frequency_ms 60000 --http_address 127.0.0.1 --http_port $port $extrargs"
 					Port = $port
 					BenchmarkSeconds = if ($_.BenchmarkSeconds) { $_.BenchmarkSeconds } else { $Cfg.BenchmarkSeconds }
 					RunBefore = $_.RunBefore
