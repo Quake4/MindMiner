a5782bf7d8ec73c65602c370062016c034867c2d
Quake4 <batsov@yandex.ru>
2023-07-30 18:17:02 +0300

    update lol miner to 1.76a

--- Miners/amd-lol-1.76a.ps1
+++ Miners/amd-lol-1.76a.ps1
@@ -44,7 +44,7 @@ $Cfg = ReadOrCreateMinerConfig "Do you want use to mine the '$Name' miner" ([IO.
 
 if (!$Cfg.Enabled) { return }
 
-$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.76/lolMiner_v1.76_Win64.zip"
+$url = "https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.76a/lolMiner_v1.76a_Win64.zip"
 $nocolor = if ([Environment]::OSVersion.Version.Major -le 6) { "--nocolor=on " } else { "" }
 
 $port_amd = [Config]::Ports[[int][eMinerType]::AMD]
