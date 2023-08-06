b8750f4bfe366ee771748e53427fad438d9121da
Quake4 <batsov@yandex.ru>
2023-08-06 11:20:10 +0300

    update xmrig to 6.20.0

--- Miners/xmrig-cpu-6.20.0.ps1
+++ Miners/xmrig-cpu-6.20.0.ps1
@@ -1,5 +1,5 @@
 <#
-MindMiner  Copyright (C) 2018-2022  Oleg Samsonov aka Quake4
+MindMiner  Copyright (C) 2018-2023  Oleg Samsonov aka Quake4
 https://github.com/Quake4/MindMiner
 License GPL-3.0
 #>
@@ -71,7 +71,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::CPU
 					API = "xmrig2"
-					URI = "https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-gcc-win64.zip"
+					URI = "https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-gcc-win64.zip"
 					Path = "$Name\xmrig.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $pools -R $($Config.CheckTimeout) --http-port=4045 --donate-level=1 --no-dmi --cpu-priority 0 $extrargs"
