64f5618394ad0af223781b8ad3e395bc14846f3c
Quake4 <batsov@yandex.ru>
2023-12-12 14:45:14 +0300

    update rigel to 1.10.1

--- Miners/nv-rigel-1.10.1.ps1
+++ Miners/nv-rigel-1.10.1.ps1
@@ -67,7 +67,7 @@ $Cfg.Algorithms | ForEach-Object {
 					Algorithm = $Algo
 					Type = [eMinerType]::nVidia
 					API = "rigel"
-					URI = "https://github.com/rigelminer/rigel/releases/download/1.10.0/rigel-1.10.0-win.zip"
+					URI = "https://github.com/rigelminer/rigel/releases/download/1.10.1/rigel-1.10.1-win.zip"
 					Path = "$Name\rigel.exe"
 					ExtraArgs = $extrargs
 					Arguments = "-a $($_.Algorithm) $hosts -u $($Pool.User) -p $($Pool.Password) -w $([Config]::WorkerNamePlaceholder) --api-bind 127.0.0.1:$port --dns-over-https --no-strict-ssl --no-watchdog --stats-interval 60 $nocolor$extrargs"
