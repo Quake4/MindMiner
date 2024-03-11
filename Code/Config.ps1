1eb08a5e19d7b13a8998ab532ab567fdbcb5172b
Quake4 <batsov@yandex.ru>
2024-03-11 09:26:38 +0300

    version 7.184

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -86,7 +86,7 @@ class Config : BaseConfig {
 	$Service = $null
 
 	static [bool] $Is64Bit = [Environment]::Is64BitOperatingSystem
-	static [string] $Version = "v7.183"
+	static [string] $Version = "v7.184"
 	static [string] $BinLocation = "Bin"
 	static [string] $MinersLocation = "Miners"
 	static [string] $PoolsLocation = "Pools"
