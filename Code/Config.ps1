89eb49d01b4cedbf17b6b93ce6839be1810d370a
Quake4 <batsov@yandex.ru>
2024-12-11 10:55:19 +0300

    remove nicehash from pools

--- Code/Config.ps1
+++ Code/Config.ps1
@@ -109,7 +109,7 @@ class Config : BaseConfig {
 	static [string] $LoginPlaceholder = "%%Login%%"
 	static [bool] $UseApiProxy = $false
 	static [string] $SMIPath = [IO.Path]::Combine([environment]::GetFolderPath([environment+SpecialFolder]::ProgramFiles), "NVIDIA Corporation\NVSMI\nvidia-smi.exe")
-	static [string] $Pools = "^(2miners|mph|nicehash|zergpool|zpool)"
+	static [string] $Pools = "^(2miners|mph|zergpool|zpool)"
 	static [int] $Max = 100
 	static [decimal] $MinSpeed = 0.01
 	static [int] $ApiSendTimeout = 55
